8738863de546d61becd38ab793179bda7666f233
Jesse Mack <Toxocious@gmail.com>
2019-08-10 14:15:51 -0700

    Minor edits to the layout_* files.
    Improved layout style. Include cbox defaults at bottom of <body>.

--- core/required/layout_bottom.php
+++ core/required/layout_bottom.php
@@ -1,2 +1,5 @@
+		<script type='text/javascript'>
+			$(".cboxElement").colorbox({ iframe: true, innerWidth: 680, innerHeight: 491, maxWidth: 686, maxHeight: 529 });
+		</script>
 	</body>
 </html>
--- core/required/layout_top.php
+++ core/required/layout_top.php
@@ -16,14 +16,16 @@
 		<script type='text/javascript' src='<?= Domain(1); ?>/js/main.js'></script>
 		<?php
 			/**
-			 * Adds snowstorm.js if the current date is sometime in December.
+			 * Adds snowstorm.js if the current month is December.
 			 */
-			if ( isBetweenDates('2018-12-01', '2018-12-31') )
+			if ( date('m') == 12 )
 			{
 				echo "<script type='text/javascript' src='" . Domain(1) . "/js/snowstorm.js'></script>";	
 			}
-		?>
 
+			if ( isset($_SESSION['abso_user']) )
+			{
+		?>
 		<script type='text/javascript' src='<?= Domain(1); ?>/js/absochat.js'></script>
 		<script type='text/javascript'>
 			ABSOLUTE_PORT = "3000";
@@ -36,8 +38,6 @@
 					postcode: "<?= $User_Data['Auth_Code']; ?>",
 					user_name: "<?= $User_Data['Username']; ?>",
 					user_id: <?= $User_Data['id']; ?>, 
-					chat_size: "0" ,
-					auto_caps: "no",
 				}
 
 				Absolute.enable();
@@ -61,98 +61,62 @@
 						return false;
 					}
 				});
-
-				<?php
-					if ( $User_Data['Power'] >= 3 )
-					{
-				?>
-					Absolute.isStaff = true;
-
-					Absolute.staff = {
-						kick: function( uID )
-						{
-							socket.emit("chaterpie-kick-user", uID, $('#chatMessage').val());
-							$('#chatMessage').val('');
-
-							Absolute.changeWindow('chat');
-							return false;
-						},
-
-						ban: function( uID, time ) {
-							if (!confirm("Do you really want to ban user #"+uID+"?"))
-							{
-								return;
-							}
-
-							var banTime = time ? time : $('#messageBanTimeStaff').val();
-							socket.emit("chaterpie-ban-user", uID, $('#chatMessage').val(), banTime);
-							$('#chatMessage').val('');
-
-							Absolute.changeWindow('chat');
-							return false;
-						},
-
-						quickBan: function( uID )
-						{
-							if ( confirm("Do you really want to ban user #"+uID+" for 5 minutes?") )
-							{
-								Absolute.staff.ban(uID, 300);
-							}
-						}
-					};
-				<?php
-					}
-				?>
 			});
 		</script>
+		<?php
+			}
+		?>
 	</head>
 
 	<body>
 		<div class='banner'>
-			<a href= <?= (isset($_SESSION['abso_user'])) ? Domain(1) . '/news.php' : Domain(1) . '/index.php' ?>>
-				<img src='<?= Domain(1); ?>/images/Assets/banner.png' />
-			</a>
-		</div>
-
-<?php
-	if ( isset($_SESSION['abso_user']) )
-	{
-?>
-
-		<div class='userbar'>
+			<?php
+				if ( isset($_SESSION['abso_user']) )
+				{
+			?>
+			
 			<div class='user'>
-				<ul>
-					<li class='dropdown' style='height: 30px; width: 198px;'>
+				<div class='avatar'>
+					<div>
+						<img src='<?= $User_Data['Avatar']; ?>' />
+					</div>
+				</div>
+
+				<div class='username'>
+					<div>
 						<a href='<?= Domain(1); ?>/profile.php?id=<?= $User_Data['id']; ?>'>
-							<?php
-								$Display_Username = $UserClass->DisplayUserName($User_Data['id'], false, true);
-
-								echo $Display_Username;
-							?>
+							<b>
+								<?php
+									$Display_Username = $User_Class->DisplayUserName($User_Data['id'], false, false);
+									echo $Display_Username;
+								?>
+							</b>
 						</a>
-						<ul class='dropdown-content'>
-							<li>
-								<a href='<?= Domain(1); ?>/settings.php'>Settings & Options</a>
-							</li>
-							<li>
-								<a href='<?= Domain(1); ?>/login.php?Logout'>Logout</a>
-							</li>
-						</ul>
-					</li>
-				</ul>
+					</div>
+				</div>
 			</div>
 
-			<div class='money'>
-				$<?= number_format($User_Data['Money']); ?>
+			<div class='stats'>
+				<div class='stat'>
+					<div>
+						$<?= number_format($User_Data['Money']); ?>
+					</div>
+				</div>
+
+				<div class='stat'>
+					<div>
+						<?= $Absolute_Time; ?>
+					</div>
+				</div>
 			</div>
-				
-			<div class='roster' id='userbar_roster'>
+
+			<div class='roster'>
 				<?php
 					for ( $i = 0; $i <= 5; $i++ )
 					{
 						if ( isset($Roster[$i]['ID']) )
 						{
-							$RosterPoke[$i] = $PokeClass->FetchPokemonData($Roster[$i]['ID']);
+							$RosterPoke[$i] = $Poke_Class->FetchPokemonData($Roster[$i]['ID']);
 						}
 						else
 						{
@@ -167,35 +131,9 @@
 						}
 
 						echo "
-							<div class='roster_slot' onmouseover='showSlot({$i});' onmouseout='hideSlot({$i});' style='text-align: center; min-width: 40px;'>
-								<div class='roster_mini'>
+							<div class='slot popup cboxElement' href='" . Domain(1) . "/core/ajax/pokemon.php?id={$RosterPoke[$i]['ID']}'>
+								<div>
 									<img src='{$RosterPoke[$i]['Icon']}' />
-								</div>
-								<div class='roster_tooltip' id='rosterTooltip{$i}'>
-						";
-
-						if ( $RosterPoke[$i]['Gender_Icon'] != null && $RosterPoke[$i]['Gender_Icon'] != 'G' && $RosterPoke[$i]['Gender_Icon'] != "(?)" )
-						{
-							echo "<img src='{$RosterPoke[$i]['Gender_Icon']}' style='height: 20px; margin: 10px 0px 0px -20px; position: absolute; width: 20px;' />";
-						}
-
-						echo "<img src='{$RosterPoke[$i]['Sprite']}' />";
-
-						if ( $RosterPoke[$i]['Item'] != null || $RosterPoke[$i]['Item'] != 0 )
-						{
-							echo "<img src='{$RosterPoke[$i]['Item_Icon']}' style='margin: 5px 0px 0px -10px; position: absolute;' />";
-						}
-
-						echo "
-									<div><b>{$RosterPoke[$i]['Display_Name']}</b></div>
-									<div class='info'>
-										<div>Level</div>
-										<div>{$RosterPoke[$i]['Level']}</div>
-									</div>
-									<div class='info'>
-										<div>Experience</div>
-										<div>" . number_format($RosterPoke[$i]['Experience']) . "</div>
-									</div>
 								</div>
 							</div>
 						";
@@ -203,35 +141,27 @@
 				?>
 			</div>
 
-			<div class='money' style='border-left: none; float: left; width: 199px;'>
-				<?= $Date; ?>
-			</div>
+			<?php
+				}
+			?>
 		</div>
-		
-	<?php
+
+<?php
+	if ( isset($_SESSION['abso_user']) )
+	{
 		if ( $User_Data['RPG_Ban'] != 1 )
 		{
+			if ( strpos($Parse_URL['path'], '/staff/') === false )
+			{
+				$Navigation->Render("Member");
+			}
+			else
+			{
+				$Navigation->Render("Staff");
+			}
 	?>
 
 		<div class='sidebar'>
-			<?php
-				if ( $User_Data['Power'] >= 3 )
-				{
-					if ( $Current_Page['URL'] != '/staff/' )
-					{
-						echo 	"<div class='button'>";
-						echo		"<a href='" . Domain(1) . "/staff'>Staff Panel</a>";
-						echo	"</div>";
-					}
-					else
-					{
-						echo 	"<div class='button'>";
-						echo		"<a href='" . Domain(1) . "/index.php'>Index</a>";
-						echo	"</div>";
-					}
-				}
-			?>
-
 			<div class='chat' id='AbsoChat'>
 				<div class='head'>
 					Chat
@@ -255,94 +185,17 @@
 			</div>
 		</div>
 
-		<?php
-			if ( $Current_Page['URL'] != "/staff/" )
-			{
-		?>
-			<div class='navigation'>
-				<ul>
-					<li class='dropdown'>
-						<a href='javascript:void(0);'>Pokemon</a>
-						<ul class='dropdown-content'>
-							<li><a href='<?= Domain(1); ?>/pokemon_center.php'>Pokemon Center</a></li>
-							<li><a href='<?= Domain(1); ?>/evolution_center.php'>Evolution Center</a></li>
-							<!--<li><a href='<?= Domain(1); ?>/lab.php'>Laboratory</a></li>-->
-						</ul>
-					</li>
-					
-					<li class='dropdown'>
-						<a href='javascript:void(0);'>Economy</a>
-						<ul class='dropdown-content'>
-							<li><a href='<?= Domain(1); ?>/trades.php'>Trade Center</a></li>
-							<li><a href='<?= Domain(1); ?>/trade_interest.php'>Trade Interest</a></li>
-							<li><a href='<?= Domain(1); ?>/shop.php'>Shops</a></li>
-						</ul>
-					</li>
-					
-					<li class='dropdown'>
-						<a href='javascript:void(0);'>Exploration</a>
-						<ul class='dropdown-content'>
-							<!--<li><a href='<?= Domain(1); ?>/faction.php'>Faction HQ</a></li>-->
-							<li><a href='<?= Domain(1); ?>/mining_select.php'>Mining Selection</a></li>
-							<li><a href='<?= Domain(1); ?>/map_select.php'>Map Selection</a></li>
-						</ul>
-					</li>
-					
-					<li class='dropdown'>
-						<a href='javascript:void(0);'>Battle</a>
-						<ul class='dropdown-content'>
-							<li><a href='<?= Domain(1); ?>/battle_trainer.php'>Battle A Trainer</a></li>
-							<li><a href='<?= Domain(1); ?>/battle_challenge.php'>Training Challenge</a></li>
-							<li><a href='<?= Domain(1); ?>/battle_regions.php'>Regional Challenges</a></li>
-							<li><a href='<?= Domain(1); ?>/battle_frontier.php'>Battle Frontier</a></li>
-						</ul>
-					</li>
-					
-					<li class='dropdown'>
-						<a href='javascript:void(0);'>Clans</a>
-						<ul class='dropdown-content'>
-							<li><a href='<?= Domain(1); ?>/clan.php'>Clan Home</a></li>
-							<li><a href='<?= Domain(1); ?>/clan_list.php'>Clan Listings</a></li>
-							<!--<li><a href='<?= Domain(1); ?>/clan_raids.php'>Clan Raids</a></li>-->
-						</ul>
-					</li>
-
-					<li class='dropdown'>
-						<a href='javascript:void(0);'>Community</a>
-						<ul class='dropdown-content'>
-							<li><a href='<?= Domain(1); ?>/forums'>Forums</a></li>
-							<li><a href='<?= Domain(1); ?>/rankings.php'>Rankings</a></li>
-							<li><a href='<?= Domain(1); ?>/messages.php'>Private Messages</a></li>
-							<li><a href='<?= Domain(1); ?>/online.php'>Online List</a></li>
-							<li><a href='<?= Domain(1); ?>/staff.php'>Staff List</a></li>
-						</ul>
-					</li>
-				</ul>
-			</div>
-		<?php
-			}
-			else
-			{
-		?>
-
-			<div class='navigation'>
-				<ul>
-					<li class='dropdown'>
-						<a href='javascript:void(0);'>Settings</a>
-						<ul class='dropdown-content'>
-							<li><a href='<?= Domain(1); ?>/staff/settings.php'>Staff Panel Settings</a></li>
-							<li><a href='<?= Domain(1); ?>/staff/message.php'>Staff Message</a></li>
-							<li><a href='<?= Domain(1); ?>/staff/maintenance.php'>Game Maintenance</a></li>
-							<li><a href='<?= Domain(1); ?>/staff/announcement.php'>Game Announcement</a></li>
-						</ul>
-					</li>
-				</ul>
-			</div>
-
-		<?php
-			}
+	<?php
 		}
 
+		/**
+		 * Check for any notifications before any further page content gets loaded.
+		 */
+		$Notification->ShowNotification($User_Data['id']);
+
+		/**
+		 * Checkto see if the page is currently under maintenance.
+		 */
 		if ( $Current_Page['Maintenance'] == 'yes' && $User_Data['Power'] >= 7 )
 		{
 			echo "<div class='maintenance'>Despite this page being down for maintenance, you seem to be authorized to be here.</div>";
