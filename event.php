3ca0eac5a5a1457d776538de100c4cfff08be669
Jesse Mack <Toxocious@gmail.com>
2019-08-10 13:22:59 -0700

    Basic event page.

---
+++ event.php
@@ -0,0 +1,18 @@
+<?php
+	require 'core/required/layout_top.php';
+?>
+
+<div class='content'>
+	<div class='head'>Event</div>
+	<div class='box'>
+		<div class='description' style='margin-bottom: 5px;'>
+			Welcome to the main page for any and all events that are held on Absolute!
+		</div>
+
+		There are currently no events being held.
+	</div>
+</div>
+
+<?php
+	require 'core/required/layout_bottom.php';
+?>
