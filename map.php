490b9a048f98904121ab0b0c12018a3fd76b8f0f
Jesse Mack <Toxocious@gmail.com>
2019-08-10 13:23:38 -0700

    Map feature files.

---
+++ map.php
@@ -0,0 +1,135 @@
+<?php
+	require 'core/required/layout_top.php';
+?>
+
+<div class='content'>
+	<div class='head'>Maps</div>
+	<div class='box'>
+		<div class='description'>
+			You may use WASD to move.
+			Hold C to run.
+		</div>
+
+		<div class='row' style='padding: 5px;'>
+
+			<div id='screen'></div>
+
+			<div class='panel' style='border-width: 2px; float: left; height: 240px; width: 240px;'>
+				<div class='panel-heading'>Map Events</div>
+				<div class='panel-body' id='map_text' style='padding: 5px;'>
+					What'll you find today?
+				</div>
+			</div>
+
+		</div>
+
+		<!--
+		<div class='row'>
+			<div class='info'>
+				manual map controls
+			</div>
+
+			<div class='info'>
+				<b>Map Level</b>: 0 omegalul<br />
+			</div>
+		</div>
+		-->
+	</div>
+</div>
+
+<!-- melonJS Library -->
+<script type="text/javascript" src="maps/Libs/melonjs.js"></script>
+<script type="text/javascript" src="maps/Libs/debugPanel.js"></script>
+
+<!-- Game Scripts -->
+<script type="text/javascript" src="maps/JS/game.js"></script>
+<script type="text/javascript" src="maps/JS/map.js"></script>
+<script type="text/javascript" src="maps/JS/tileinfo.js"></script>
+<script type="text/javascript" src="maps/JS/movement.js"></script>
+<script type="text/javascript" src="maps/JS/network.js"></script>
+<script type="text/javascript" src="maps/JS/encounter.js"></script>
+<script type="text/javascript" src="maps/JS/fishing.js"></script>
+<script type="text/javascript" src="maps/JS/actions.js"></script>
+<script type="text/javascript" src="maps/resources.js"></script>
+
+<!-- Entities -->
+<script type="text/javascript" src="maps/JS/entities/entities.js"></script>
+<script type="text/javascript" src="maps/JS/entities/static.js"></script>
+<script type="text/javascript" src="maps/JS/entities/transport.js"></script>
+<script type="text/javascript" src="maps/JS/entities/HUD.js"></script>
+
+<!-- Screens -->
+<script type="text/javascript" src="maps/JS/screens/error.js"></script>
+<script type="text/javascript" src="maps/JS/screens/title.js"></script>
+<script type="text/javascript" src="maps/JS/screens/play.js"></script>
+<script type="text/javascript" src="maps/JS/screens/loading.js"></script>
+<script type="text/javascript" src="maps/JS/screens/crash.js"></script>
+
+
+<!-- Load the canvas when the DOM has finished loading. -->
+<script type="text/javascript">
+	me.device.onReady(function onReady() {
+		game.onload();
+	});
+</script>
+
+<!-- Temporary styling? -->
+<style>
+	#screen canvas
+	{
+		border: 2px solid #4A618F;
+		border-radius: 4px;
+
+		display: block;
+		float: left;
+
+		margin: 0px 10px;
+
+		height: 240px;
+		width: 240px;
+	}
+
+	#MapControls
+	{
+		border: 2px solid #4A618F;
+		border-radius: 4px;
+
+		margin-right: 10px;
+
+		width: 240px;
+	}
+
+	#MapText
+	{
+		border: 2px solid #4A618F;
+		border-radius: 4px;
+
+		display: block;
+		float: left; 
+
+		height: 240px;
+		width: 240px;
+	}
+
+	.row
+	{
+		margin: 0 auto !important;
+
+		padding: 5px;
+
+		width: 510px;
+	}
+
+	.row .info
+	{
+		border: 2px solid #4A618F;
+		border-radius: 4px;
+		float: left;
+		margin: 0px 0px 0px 10px;
+		padding: 5px 0px;
+		width: 240px;
+	}
+</style>
+
+<?php
+	require 'core/required/layout_bottom.php';
---
+++ map_select.php
@@ -0,0 +1,100 @@
+<?php
+	/**
+	 * Proceeding to warp to a map.
+	 */
+	if ( isset($_POST['Explore']) )
+	{
+		require 'core/required/session.php';
+
+		unset($_SESSION['Maps']);
+
+		$Map = $Purify->Cleanse($_POST['Explore']);
+
+		//try
+		//{
+		//	$Fetch_Map_Data = $PDO->prepare("SELECT `start_pos` FROM `maps` WHERE `name` = ? LIMIT 1");
+		//	$Fetch_Map_Data->execute([ $Map ]);
+		//	$Fetch_Map_Data->setFetchMode(PDO::FETCH_ASSOC);
+		//	$Map_Data = $Fetch_Map_Data->fetch();
+		//}
+		//catch( PDOException $e )
+		//{
+		//	HandleError( $e->getMessage() );
+		//}
+		//
+		//$Spawn_Pos = explode(',', $Map_Data['start_pos']);
+		//
+		$_SESSION['Maps']['Quip'] 		= "What'll you find today?";
+		$_SESSION['Maps']['Map_ID']		= $Map;
+
+		echo "<pre>";var_dump($_SESSION['Maps']);echo "</pre>";
+		//exit;
+
+		header("Location: map.php");
+
+		exit;
+	}
+
+	require 'core/required/layout_top.php';
+
+	/**
+	 * Fetch all available maps.
+	 */
+	try
+	{
+		$Fetch_Maps = $PDO->prepare("SELECT * FROM `maps` WHERE `id` != 0");
+		$Fetch_Maps->execute();
+		$Fetch_Maps->setFetchMode(PDO::FETCH_ASSOC);
+		$Maps = $Fetch_Maps->fetchAll();
+	}
+	catch( PDOException $e )
+	{
+		HandleError( $e->getMessage() );
+	}
+?>
+
+<div class='content'>
+	<div class='head'>Explore A Map</div>
+	<div class='box'>
+		<div class='row'>
+			<?php
+				foreach ( $Maps as $Key => $Value )
+				{
+					echo "
+						<div class='panel' style='float: left; width: 49.4%;'>
+							<div class='panel-heading'>{$Value['display_name']}</div>
+							<div class='panel-body' style='padding: 2px;'>
+								<div style='border: 2px solid #000; border-radius: 4px; float: left; height: 204px; margin-bottom: 5px; width: 204px;'>
+									<img src='images/Maps/{$Value['name']}.png' style='height: 200px; width: 200px;' />
+								</div>
+								<div style='float: left; height: 204px; padding: 5px; width: 224px;'>
+									<div style='height: 155px;'>
+										{$Value['description']}
+									</div>
+									<div>
+										" . nl2br($Value['credits']) . "
+									</div>
+								</div>
+
+								<form method='POST' action='map_select.php'>
+									<input type='hidden' name='Explore' value='{$Value['name']}' />
+									<input type='submit' style='padding: 5px; width: 100%;' value='Explore!' />	
+								</form>
+							</div>
+						</div>
+					";
+				}
+			?>
+		</div>
+	</div>
+</div>
+
+<style>
+	.box .row .panel:nth-child(odd)
+	{
+		margin-right: 5px;
+	}
+</style>
+
+<?php
+	require 'core/required/layout_bottom.php';
