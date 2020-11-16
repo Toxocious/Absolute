ff8316138fde61ef6441d1eb30c7f0935f475f5f
Toxocious <Toxocious@gmail.com>
2020-11-16 07:51:21 -0800

    Minor URL fix for the defined domains.

--- core/required/domains.php
+++ core/required/domains.php
@@ -5,11 +5,11 @@
   if ( $_SERVER['HTTP_HOST'] === 'localhost' )
   {
     define("DOMAIN_ROOT", "https://localhost");
-    define("DOMAIN_SPRITES", "https://localhost/images/Sprites");
+    define("DOMAIN_SPRITES", "https://localhost/images");
   }
   // Live server domains.
   else
   {
     define("DOMAIN_ROOT", "https://absoluterpg.com");
-    define("DOMAIN_SPRITES", "https://absoluterpg.com/images/Sprites");
+    define("DOMAIN_SPRITES", "https://absoluterpg.com/images");
   }
