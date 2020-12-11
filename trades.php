efebfb8f0459f3b0824475039642b50451109892
Toxocious <Toxocious@gmail.com>
2020-12-10 16:38:39 -0800

    [Trades] Fix various interfacing issues among the primary trade pages.

--- trades.php
+++ trades.php
@@ -7,93 +7,130 @@
 	<div class='body'>
 		<div class='nav'>
 			<div>
-				<a href='<?= DOMAIN_ROOT; ?>/shop.php?Shop=pokemon' style='display: block;'>Pokemon</a>
-			</div>
-		</div>
-		<div class='nav'>
-			<div>
-				<a href='#' onclick="Tab('pending');">
+				<a href='javascript:void(0);' onclick="Tab('pending');">
 					Pending Trades
 				</a>
 			</div>
 			<div>
-				<a href='#' onclick="Tab('history');">
+				<a href='javascript:void(0);' onclick="Tab('history');">
 					Trade History
 				</a>
 			</div>
 		</div>
 
-		<div id='TradeAJAX'>
+		<div id='TradeAJAX' style='display: flex; flex-direction: row; flex-wrap: wrap; justify-content: center;'>
 			<?php
 				if ( !isset($_GET['Action']) && !isset($_GET['id']) )
 				{
 			?>
 			
-				<div class='description' style='margin-bottom: 5px;'>Enter a user's ID to begin a trade with them.</div>
-				<div class='panel' style='margin-top: 5px;'>
-					<div class='head'>Create A Trade</div>
-					<div class='body' style='padding: 5px;'>
-						<input type='text' placeholder='User ID' id='recipientID' style='text-align: center; width: 200px;'/><br />
-						<button onclick='TradePrepare();' style='width: 200px;'>Begin A Trade</button>
-					</div>
+				<div data-id='left-col' style='flex-basis: 49%; margin: 5px 3px;'>
+					<div class='description'>Enter another user's ID to begin a trade with them.</div>
+
+					<table class='border-gradient' style='width: 100%;'>
+						<thead>
+							<tr>
+								<th colspan='1'>
+									Create A Trade
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>
+									<input type='text' placeholder='User ID' id='recipientID' style='text-align: center; width: 200px;'/>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<button onclick='TradePrepare();' style='width: 200px;'>
+										Begin A Trade
+									</button>
+								</td>
+							</tr>
+						</tbody>
+					</table>
 				</div>
 
-				<div class='description' style='margin: 5px 0px;'>All ingoing and outgoing trades that involve you are listed below.</div>
-				<div class='panel' style='margin-top: 5px;'>
-					<div class='head'>Pending Trades</div>
-					<div class='body' style='padding: 5px;'>
-						<?php
-							try
-							{
-								$Pending_Query = $PDO->prepare("SELECT `ID`, `Sender`, `Receiver`, `Status` FROM `trades` WHERE (`Sender` = ? OR `Receiver` = ?) AND `Status` = ?");
-								$Pending_Query->execute([ $User_Data['id'], $User_Data['id'], 'Pending' ]);
-								$Pending_Query->setFetchMode(PDO::FETCH_ASSOC);
-								$Pending_Trades = $Pending_Query->fetchAll();
-							}
-							catch( PDOException $e )
-							{
-								HandleError( $e->getMessage() );
-							}
+				<div data-id='right-col' style='flex-basis: 49%; margin: 5px 3px;'>
+					<div class='description'>All pending trades that involve you are listed below.</div>
 
-							if ( count($Pending_Trades) === 0 )
+					<?php
+						try
+						{
+							$Pending_Query = $PDO->prepare("SELECT `ID`, `Sender`, `Receiver`, `Status` FROM `trades` WHERE (`Sender` = ? OR `Receiver` = ?) AND `Status` = ?");
+							$Pending_Query->execute([ $User_Data['id'], $User_Data['id'], 'Pending' ]);
+							$Pending_Query->setFetchMode(PDO::FETCH_ASSOC);
+							$Pending_Trades = $Pending_Query->fetchAll();
+						}
+						catch( PDOException $e )
+						{
+							HandleError( $e->getMessage() );
+						}
+
+						if ( count($Pending_Trades) === 0 )
+						{
+							$Trade_Text = "
+								<tr>
+									<td colspan='3' style='padding: 7px;'>
+										You do not currently have any pending trades.
+									</td>
+								</tr>
+							";
+						}
+						else
+						{
+							$Trade_Text = "
+								<tbody>
+									<td colspan='1' style='padding: 7px;'>
+										<b>Trade ID</b>
+									</td>
+									<td colspan='1' style='padding: 7px;'>
+										<b>Sender</b>
+									</td>
+									<td colspan='1' style='padding: 7px;'>
+										<b>Recipient</b>
+									</td>
+								</tbody>
+							";
+
+							foreach( $Pending_Trades as $Key => $Value )
 							{
-								echo "<div class='notice'>You do not currently have any pending trades.</div>";
-							}
-							else
-							{
-								echo "
-									<table class='standard' style='margin: 0px auto; width: calc(100% - 10px);'>
-										<thead>
-											<th style='width: 25%;'>Trade ID</th>
-											<th style='width: 25%;'>Sender</th>
-											<th style='width: 25%;'>Recipient</th>
-											<th style='width: 25%;'>Status</th>
-										</thead>
-										<tbody>
-								";
-								foreach( $Pending_Trades as $Key => $Value )
-								{
-									$Sender = $User_Class->FetchUserData($Value['Sender']);
-									$Sender_Username = $User_Class->DisplayUserName($Sender['ID']);
-									$Recipient = $User_Class->FetchUserData($Value['Receiver']);
-									$Recipient_Username = $User_Class->DisplayUserName($Recipient['ID']);
-	
-									echo "
-										<tr>
-											<td><a href='javascript:void(0);' onclick='TradeView({$Value['ID']});'>#" . number_format($Value['ID']) . "</a></td>
-											<td><a href='" . DOMAIN_ROOT . "/profile.php?id={$Sender['ID']}'>{$Sender_Username}</a></td>
-											<td><a href='" . DOMAIN_ROOT . "/profile.php?id={$Recipient['ID']}'>{$Recipient_Username}</a></td>
-											<td style='color: #888;'>{$Value['Status']}</td>
-										</tr>
-									";
-								}
-								echo "
-										</tbody>
-									</table>
+								$Sender = $User_Class->FetchUserData($Value['Sender']);
+								$Sender_Username = $User_Class->DisplayUserName($Sender['ID']);
+
+								$Recipient = $User_Class->FetchUserData($Value['Receiver']);
+								$Recipient_Username = $User_Class->DisplayUserName($Recipient['ID']);
+
+								$Trade_Text .= "
+									<tr>
+										<td style='padding: 6px;'>
+											<a href='javascript:void(0);' onclick='TradeView({$Value['ID']});'>#" . number_format($Value['ID']) . "</a>
+										</td>
+										<td style='padding: 6px;'>
+											<a href='" . DOMAIN_ROOT . "/profile.php?id={$Sender['ID']}'>{$Sender_Username}</a>
+										</td>
+										<td style='padding: 6px;'>
+											<a href='" . DOMAIN_ROOT . "/profile.php?id={$Recipient['ID']}'>{$Recipient_Username}</a>
+										</td>
+									</tr>
 								";
 							}
-						?>
-					</div>
+						}
+					?>
+
+					<table class='border-gradient' style='width: 100%;'>
+						<thead>
+							<tr>
+								<th colspan='3'>
+									Pending Trades
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?= $Trade_Text; ?>
+						</tbody>
+					</table>
 				</div>
 
 			<?php
