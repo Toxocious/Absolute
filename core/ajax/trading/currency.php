4a341a1f953cb3d95cd89657d7cf0883e37d2065
Toxocious <Toxocious@gmail.com>
2020-12-11 17:34:07 -0800

    [Trades] Fix handling displaying and adding currencies.

--- core/ajax/trading/currency.php
+++ core/ajax/trading/currency.php
@@ -1,45 +1,41 @@
 <?php
 	require '../../required/session.php';
+
+	if ( !isset($_POST['id']) )
+  {
+    echo "
+      <tr>
+        <td colspan='21'>
+          <b style='color: #f00'>
+            An error has occurred while fetching this trainer's currencies.
+        </td>
+      </tr>
+    ";
+
+    return;
+  }
 	
-	if ( isset($User_Data) )
+	$User_ID = $Purify->Cleanse($_POST['id']);
+	$User = $User_Class->FetchUserData($User_ID);
+
+	foreach ( $Constants->Currency as $Key => $Value )
 	{
-		if ( isset($_POST['id']) )
+		if ( $Value['Tradeable'] )
 		{
-			$User_ID = $Purify->Cleanse($_POST['id']);
-			$User = $User_Class->FetchUserData($User_ID);
-
-			foreach ( $Constants->Currency as $Key => $Value )
-			{
-				if ( $Value['Tradeable'] )
-				{
-					echo "
-						<div>
-							<div style='float: left; width: 120px;'>
-								<img src='{$Value['Icon']}' style='height: 32px; width: 32px;' /><br />
-								{$Value['Name']}
-							</div>
-
-							<div style='float: left; width: 182px;'>
-								" . number_format($User[$Value['Value']]) . "<br />
-								<input type='text' id='{$Value['Value']}' placeholder='0' style='border: none; border-radius: 0px; padding: 5px 0px; margin: 1px; text-align: center; width: 179px;' />
-							</div>
-
-							<div style='float: left; padding: 12px 15px;'>
-								<!-- Action(User_ID, Action, Type, Data) -->
-								<button onclick='Action($User_ID, \"Add\", \"Currency\", \"{$Value['Value']}\")'>Add To Trade</button>
-							</div>
-						</div>
-						<hr />
-					";
-				}
-			}
+			echo "
+				<tr>
+					<td colspan='7'>
+						<img src='{$Value['Icon']}' />
+						<br />
+						" . number_format($User[$Value['Value']]) . "
+					</td>
+					<td colspan='7'>
+						<input type='text' id='User-{$User_ID}-{$Value['Value']}' placeholder='0' style='width: 50%;' />
+					</td>
+					<td colspan='7'>
+						<button style='width: 50px;' onclick='Action($User_ID, \"Add\", \"Currency\", \"User-{$User_ID}-{$Value['Value']}\")'>Add</button>
+					</td>
+				</tr>
+			";
 		}
-		else
-		{
-			echo "An error has occurred.";
-		}
-	}
-	else
-	{
-		echo "To use this feature, you must be logged in.";
 	}
