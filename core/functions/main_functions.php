2215dfe69d49cd8aca75d750102e5f13f443f325
Toxocious <Toxocious@gmail.com>
2021-01-15 19:47:33 -0800

    Refactor the 'Purify' function to return the input as the original data type

--- core/functions/main_functions.php
+++ core/functions/main_functions.php
@@ -68,26 +68,51 @@
 	 * Filters user inputs.
 	 * Add more parameters later on for more diversity.
 	 */
-	function Purify($input)
+	function Purify($Input)
 	{
-		$text = $input;
+		if ( !$Input )
+			return false;
+		
+		$Input_Type = gettype($Input);
+		$Input_As_Text = $Input;
 
-		if ( is_array($text) )
+		if ( is_array($Input_As_Text) )
 		{
-			foreach ( $text as $key => $T )
+			foreach ( $Input_As_Text as $K => $V )
 			{
-				$T = htmlentities($T, ENT_NOQUOTES, "UTF-8");
-				$T = nl2br($T, false);
-				$text[$key] = $T;
+				$V = htmlentities($V, ENT_NOQUOTES, "UTF-8");
+				$V = nl2br($V, false);
+				$Input_As_Text[$K] = $V;
 			}
 		}
 		else
 		{
-			$text = htmlentities($text, ENT_NOQUOTES, "UTF-8");
-			$text = nl2br($text, false);
+			$Input_As_Text = htmlentities($Input_As_Text, ENT_NOQUOTES, "UTF-8");
+			$Input_As_Text = nl2br($Input_As_Text, false);
 		}
 
-		return $text;
+		/**
+		 * Return the variable as it's original type.
+		 */
+		switch ( $Input_Type )
+		{
+			case 'boolean':
+				return (bool) $Input_As_Text;
+			case 'integer':
+				return (integer) $Input_As_Text;
+			case 'double':
+				return (double) $Input_As_Text;
+			case 'string':
+				return (string) $Input_As_Text;
+			case 'array':
+				return (array) $Input_As_Text;
+			case 'object':
+				return (object) $Input_As_Text;
+			case 'NULL':
+				return null;
+			case 'unknown type':
+				return false;
+		}
 	}
 
 	/**
