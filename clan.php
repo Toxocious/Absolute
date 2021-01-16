498dce51db22a9ab9092a9e7413e4a32312aaf37
Toxocious <Toxocious@gmail.com>
2021-01-15 19:45:36 -0800

    [Clan] Add link to clan dm page

--- clan.php
+++ clan.php
@@ -172,7 +172,7 @@
 								<a href='<?= DOMAIN_ROOT; ?>/core/ajax/clan/.php'>Clan Upgrades</a>
 							</td>
 							<td colspan='1' style='width: 50%;'>
-								<a href='<?= DOMAIN_ROOT; ?>/core/ajax/clan/.php'>Send A Clan-wide PM</a>
+								<a href='<?= DOMAIN_ROOT; ?>/core/ajax/clan/send_message.php'>Send A Clan-wide PM</a>
 							</td>
 						</tr>
 					</tbody>
