46d2d0dd882d502db78285ba9da82d9ec20099b8
Toxocious <Toxocious@gmail.com>
2021-01-16 18:05:52 -0800

    [Layout] Properly style and vertically center icons
    Styling was previously done in-line on the element itself, and is now moved to stylesheets. Was previously offset higher by 6px, and is now fixed.

--- core/required/layout_bottom.php
+++ core/required/layout_bottom.php
@@ -2,24 +2,28 @@
 			
 			<footer>
 				<div class='copyright'>
-					Pok&eacute;mon Absolute &copy; 2018 - <?= date("Y"); ?> Toxocious<br />
-					Pok&eacute;mon &copy; 1995-<?=date("Y"); ?> Nintendo/Creatures Inc./Game Freak Inc, please support the <a href="http://pokemon.com" target="_blank">official release.</a>
+					Pok&eacute;mon Absolute &copy; 2018 - <?= date('Y'); ?> Toxocious<br />
+					Pok&eacute;mon &copy; 1995 - <?= date('Y'); ?> Nintendo/Creatures Inc./Game Freak Inc, please support the <a href='http://pokemon.com' target='_blank'>official release.</a>
 				</div>
 
 				<div class='social-links'>
-					<a target="_blank" rel="noopener noreferrer" href="https://github.com/toxocious" style="color: rgb(250, 250, 250);">
-						<svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" viewBox="0 0 47.999998 48.000002" width="34" height="34">
-						<style id="style3">
-							.st0{transition: all 150ms;}.st0:hover{fill:#fff;}
-						</style>
-						<path class="st0" d="M23.928 1.15C11 1.15.514 11.638.514 24.566c0 10.343 6.75 19.105 15.945 22.265 1.148.144 1.58-.574 1.58-1.15v-4.02c-6.465 1.436-7.902-3.16-7.902-3.16-1.005-2.73-2.586-3.45-2.586-3.45-2.154-1.435.144-1.435.144-1.435 2.298.144 3.59 2.442 3.59 2.442 2.156 3.59 5.46 2.586 6.753 2.01.142-1.58.86-2.585 1.435-3.16-5.17-.574-10.63-2.585-10.63-11.635 0-2.585.862-4.596 2.442-6.32-.287-.575-1.005-3.017.288-6.177 0 0 2.01-.574 6.464 2.442 1.866-.574 3.877-.718 5.888-.718 2.01 0 4.022.286 5.89.717 4.453-3.016 6.464-2.442 6.464-2.442 1.293 3.16.43 5.602.287 6.177a9.29 9.29 0 0 1 2.44 6.32c0 9.05-5.458 10.918-10.63 11.492.863.718 1.58 2.155 1.58 4.31v6.464c0 .574.432 1.292 1.58 1.15 9.338-3.16 15.946-11.924 15.946-22.266-.143-12.785-10.63-23.27-23.558-23.27z" id="path20" clip-rule="evenodd" fill="#4A618F" fill-rule="evenodd"/></svg>
+					<a href='https://github.com/toxocious' target='_blank' rel='noopener noreferrer' style='color: rgb(250, 250, 250);'>
+						<svg class='icon' viewBox='0 0 47.999998 48.000002' xmlns='http://www.w3.org/2000/svg'>
+							<path
+								class='icon'
+								data-icon='github'
+								d='M23.928 1.15C11 1.15.514 11.638.514 24.566c0 10.343 6.75 19.105 15.945 22.265 1.148.144 1.58-.574 1.58-1.15v-4.02c-6.465 1.436-7.902-3.16-7.902-3.16-1.005-2.73-2.586-3.45-2.586-3.45-2.154-1.435.144-1.435.144-1.435 2.298.144 3.59 2.442 3.59 2.442 2.156 3.59 5.46 2.586 6.753 2.01.142-1.58.86-2.585 1.435-3.16-5.17-.574-10.63-2.585-10.63-11.635 0-2.585.862-4.596 2.442-6.32-.287-.575-1.005-3.017.288-6.177 0 0 2.01-.574 6.464 2.442 1.866-.574 3.877-.718 5.888-.718 2.01 0 4.022.286 5.89.717 4.453-3.016 6.464-2.442 6.464-2.442 1.293 3.16.43 5.602.287 6.177a9.29 9.29 0 0 1 2.44 6.32c0 9.05-5.458 10.918-10.63 11.492.863.718 1.58 2.155 1.58 4.31v6.464c0 .574.432 1.292 1.58 1.15 9.338-3.16 15.946-11.924 15.946-22.266-.143-12.785-10.63-23.27-23.558-23.27z'
+								clip-rule='evenodd'
+								fill-rule='evenodd'
+							/>
+						</svg>
 					</a>
 				</div>
 			</footer>
 		</div>
 		
 		<script type='text/javascript'>
-			$(".cboxElement").colorbox({ iframe: true, innerWidth: 680, innerHeight: 491, maxWidth: 686, maxHeight: 529 });
+			$('.cboxElement').colorbox({ iframe: true, innerWidth: 680, innerHeight: 491, maxWidth: 686, maxHeight: 529 });
 		</script>
 	</body>
 </html>
