38bea09c51214632592fc67ce92195738e2a85ec
Toxocious <Toxocious@gmail.com>
2021-02-22 18:34:53 -0800

    [Shops] Refactor the main /shop.php page
    Now includes much better support via a dedicated class, and the code is much cleaner.

--- shop.php
+++ shop.php
@@ -1,89 +1,70 @@
 <?php
-	require 'core/required/layout_top.php';
-	require 'core/functions/shop.php';
+	require_once 'core/required/layout_top.php';
+	require_once 'core/classes/shop.php';
 
-	if ( isset($_GET['shop']) )
-	{
-		$Shop = Purify($_GET['shop']);
-	}
+	if ( isset($_GET['Shop']) )
+		$Shop_ID = Purify($_GET['Shop']);
 	else
-	{
-		$Shop = 'pokemon';
-	}
+		$Shop_ID = 1;
 
-	/**
-	 * Switch/Case for all shops.
-	 */
-	switch($Shop)
-	{
-		case 'pokemon':
-			$Shop = "pokemon_shop";
-			$Description = "Welcome to the Pokemon Shop.";
-			$Shiny_Odds = $Constants->Shiny_Odds[$Shop];
-			break;
-	}
+	$Shop = $Shop_Class->FetchShopData($Shop_ID);
 ?>
 
 <div class='panel content'>
-	<div class='head'>Shops</div>
+	<div class='head'><?= ($Shop ? $Shop['Name'] : 'Shop'); ?></div>
 	<div class='body'>
+		<?php
+			if ( !$Shop )
+			{
+				echo "
+					<div style='margin: auto; padding: 10px;'>
+						An error occurred while loading the shop.
+					</div>
+				";
+
+				require_once 'core/required/layout_bottom.php';
+
+				return;
+			}
+		?>
+
 		<div class='nav'>
 			<div>
-				<a href='<?= DOMAIN_ROOT; ?>/shop.php?Shop=pokemon' style='display: block;'>Pokemon</a>
+				<a href='<?= DOMAIN_ROOT; ?>/shop.php?Shop=1' style='display: block;'>Pokemon</a>
 			</div>
 		</div>
 
-		<div id='result' style='display: none; height: 103px; margin-bottom: 5px; width: 80%;'></div>
-
-		<div class='description' style='margin-bottom: 5px;'>
-			<?= $Description; ?>
+		<div class='description'>
+			<?= $Shop['Description']; ?>
 		</div>
 
-		<div class='row' style='display: flex; flex-direction: row; flex-wrap: wrap; justify-content: center;'>
+		<div id='ShopAJAX'></div>
+
+		<?php
+			$Selling_Pokemon = $Shop_Class->FetchShopPokemon($Shop_ID);
+			if ( $Selling_Pokemon )
+			{
+		?>
+
+		<div class='flex wrap' style='justify-content: center;'>
 			<?php
-				try
+				foreach ( $Selling_Pokemon as $Shop_Pokemon )
 				{
-					$Fetch_Obtainables = $PDO->prepare("SELECT * FROM `obtainable_pokemon` WHERE `Obtained_Place` = ? AND `Active` = 'Yes' ORDER BY `Pokedex_ID` ASC");
-					$Fetch_Obtainables->execute([ $Shop ]);
-					$Fetch_Obtainables->setFetchMode(PDO::FETCH_ASSOC);
-					$Obtainables = $Fetch_Obtainables->fetchAll();
-				}
-				catch ( PDOException $e )
-				{
-					HandleError( $e->getMessage() );
-				}
-
-				foreach( $Obtainables as $Key => $Value )
-				{
-					// No price has been set; don't list it.
-					if ( !$Value['Prices'] )
-					{
+					if ( !$Shop_Pokemon['Prices'] )
 						continue;
-					}
 
-					$Pokemon = $Poke_Class->FetchPokedexData($Value['Pokedex_ID'], $Value['Alt_ID'], $Value['Type']);
-
-					if ( $Value['Type'] !== "Normal" )
-					{
-						$Pokemon['Name'] = $Value['Type'] . $Pokemon['Name'];
-					}
-
-					/**
-					 * Fetch the required prices of the object.
-					 */
-					$Price_Array = FetchPriceList($Value['Prices']);
-
-					/**
-					 * Determine if the user is eligible to purchase the object.
-					 */
 					$Can_Afford = true;
 					$Price_String = '';
+
+					$Pokedex_Data = $Poke_Class->FetchPokedexData($Shop_Pokemon['Pokedex_ID'], $Shop_Pokemon['Alt_ID'], $Shop_Pokemon['Type']);
+						
+					$Price_Array = $Shop_Class->FetchPriceList($Shop_Pokemon['Prices']);
 					foreach ( $Price_Array[0] as $Currency => $Amount )
 					{
-						// Append to the price string for output later.
-						$Price_String .= "<img src='" . DOMAIN_SPRITES . "/Assets/{$Currency}.png' /> " . number_format($Amount) . "<br />";
+						$Price_String .= "
+							<img src='" . DOMAIN_SPRITES . "/Assets/{$Currency}.png' />" . number_format($Amount) . "<br />
+						";
 
-						// Now check to see if the user can afford it.
 						if ( $User_Data[$Currency] < $Amount )
 						{
 							$Can_Afford = false;
@@ -95,13 +76,10 @@
 						}
 					}
 
-					/**
-					 * Generate the button to purchase the Pokemon.
-					 */
 					if ( $Can_Afford )
 					{
 						$Purchase_Button = "
-							<button onclick='Purchase({$Value['ID']});'>
+							<button onclick='Purchase({\"ID\": {$Shop_Pokemon['ID']}, \"Type\": \"Pokemon\"});'>
 								Purchase
 							</button>
 						";
@@ -110,56 +88,213 @@
 					{
 						$Purchase_Button = "
 							<button class='disabled'>
-								Can't Purchase
+								Can't Afford
+							</button>
+						";
+					}
+
+					if ( $Shop_Pokemon['Remaining'] < 1 )
+					{
+						$Purchase_Button = "
+							<button class='disabled'>
+								Not In Stock
 							</button>
 						";
 					}
 
 					echo "
-						<div class='panel' style='flex-basis: calc(100% / 4 - 12px); margin: 3px;'>
-							<div class='head'>{$Pokemon['Name']}</div>
-							<div class='body' style='display: flex; flex-direction: row; flex-wrap: wrap; justify-content: center; padding: 5px;'>
-								<div style='flex-basis: 100%; text-align: center;'>
-									{$Purchase_Button}
-								</div>
-								<br />
-								<div style='flex-basis: 50%;'>
-									<img src='{$Pokemon['Sprite']}' />
-								</div>
-								<div style='flex-basis: 50%;'>
-									{$Price_String}
-								</div>
-							</div>
-						</div>
+						<table class='border-gradient' style='flex-basis: 200px; margin: 5px 5px;'>
+							<thead>
+								<tr>
+									<th colspan='2'>
+										{$Pokedex_Data['Display_Name']}
+									</th>
+								</tr>
+							</thead>
+
+							<tbody>
+								<tr>
+									<td colspan='1' style='width: 96px;'>
+										<img src='{$Pokedex_Data['Sprite']}' />
+									</td>
+									<td colspan='1'>
+										{$Price_String}
+									</td>
+								</tr>
+							</tbody>
+							<tbody>
+								<tr>
+									<td colspan='2' id='Pokemon_{$Shop_Pokemon['ID']}'>
+									" . ($Shop_Pokemon['Remaining'] < 1 ? "Out of stock!" : "In stock: " . number_format($Shop_Pokemon['Remaining'])) . "
+									</td>
+								</tr>
+							</tbody>
+							<tbody>
+								<tr>
+									<td colspan='2'>
+										{$Purchase_Button}
+									</td>
+								</tr>
+							</tbody>
+						</table>
 					";
 				}
 			?>
 		</div>
+
+		<?php
+			}
+
+			$Selling_Items = $Shop_Class->FetchShopItems($Shop_ID);
+			if ( $Selling_Items )
+			{
+		?>
+
+		<div class='flex wrap' style='justify-content: center;'>
+			<?php
+				foreach ( $Selling_Items as $Shop_Items )
+				{
+					if ( !$Shop_Items['Prices'] )
+						continue;
+
+					$Can_Afford = true;
+					$Price_String = '';
+
+					$Price_Array = $Shop_Class->FetchPriceList($Shop_Items['Prices']);
+					foreach ( $Price_Array[0] as $Currency => $Amount )
+					{
+						$Price_String .= "
+							<img src='" . DOMAIN_SPRITES . "/Assets/{$Currency}.png' />" . number_format($Amount) . "<br />
+						";
+
+						if ( $User_Data[$Currency] < $Amount )
+						{
+							$Can_Afford = false;
+							break;
+						}
+						else
+						{
+							$Can_Afford = true;
+						}
+					}
+
+					if ( $Can_Afford )
+					{
+						$Purchase_Button = "
+							<button onclick='Purchase({\"ID\": {$Shop_Items['ID']}, \"Type\": \"Item\"});'>
+								Purchase
+							</button>
+						";
+					}
+					else
+					{
+						$Purchase_Button = "
+							<button class='disabled'>
+								Can't Afford
+							</button>
+						";
+					}
+
+					if ( $Shop_Items['Remaining'] < 1 )
+					{
+						$Purchase_Button = "
+							<button class='disabled'>
+								Not In Stock
+							</button>
+						";
+					}
+
+					echo "
+						<table class='border-gradient' style='flex-basis: 200px; margin: 5px 5px;'>
+							<thead>
+								<tr>
+									<th colspan='2'>
+										{$Pokedex_Data['Display_Name']}
+									</th>
+								</tr>
+							</thead>
+
+							<tbody>
+								<tr>
+									<td colspan='1'>
+										<img src='{$Pokedex_Data['Sprite']}' />
+									</td>
+									<td colspan='1'>
+										{$Price_String}
+									</td>
+								</tr>
+							</tbody>
+							<tbody>
+								<tr>
+									<td colspan='2'>
+										{$Purchase_Button}
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					";
+				}
+			?>
+		</div>
+
+		<?php
+			}
+		?>
+
 	</div>
 </div>
 
 <script type='text/javascript'>
-	function Purchase(id)
+	const Purchase = (Object) =>
 	{
-		$.ajax({
-			type: "POST",
-			url: "<?= DOMAIN_ROOT; ?>/core/ajax/shop.php",
-			data: { id: id, shop: '<?= substr($Shop, 0, -5); ?>' },
-			success: function(data)
-			{
-				$('#result').html(data);
-				$('#result').addClass('success');
-				$('#result').css('display', 'block');
-			},
-			error: function(data)
-			{
-				$('#result').html(data);
-				$('#result').addClass('error');
-				$('#result').css('display', 'block');
-			}
-		});
+		return new Promise((resolve, reject) =>
+    {
+      const req = new XMLHttpRequest();
+      req.open('GET', `<?= DOMAIN_ROOT; ?>/core/ajax/shop/purchase.php?Shop=<?= $Shop['ID']; ?>&Object_ID=${Object.ID}&Object_Type=${Object.Type}`);
+      req.send(null);
+      req.onerror = (error) => reject(`Network Error: ${error}`);
+      req.onload = () =>
+      {
+        if ( req.status === 200 )
+        {
+          document.querySelector('#ShopAJAX').innerHTML = req.responseText;
+					FetchStock(Object);
+          resolve(req.response);
+        }
+        else
+        {
+          document.querySelector('#ShopAJAX').innerHTML = req.statusText;
+					FetchStock(Object);
+          reject(req.statusText);
+        }
+      };
+    });
+	}
+
+	const FetchStock = (Object) =>
+	{
+		return new Promise((resolve, reject) =>
+    {
+      const req = new XMLHttpRequest();
+      req.open('GET', `<?= DOMAIN_ROOT; ?>/core/ajax/shop/fetch_stock.php?Object_ID=${Object.ID}&Object_Type=${Object.Type}`);
+      req.send(null);
+      req.onerror = (error) => reject(`Network Error: ${error}`);
+      req.onload = () =>
+      {
+        if ( req.status === 200 )
+        {
+          document.querySelector(`#${Object.Type}_${Object.ID}`).innerHTML = req.responseText;
+          resolve(req.response);
+        }
+        else
+        {
+          document.querySelector(`#${Object.Type}_${Object.ID}`).innerHTML = req.statusText;
+          reject(req.statusText);
+        }
+      };
+    });
 	}
 </script>
 
 <?php
-	require 'core/required/layout_bottom.php';
+	require_once 'core/required/layout_bottom.php';
