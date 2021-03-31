304a878e828b88df85c082237b7bf24a90efba30
Toxocious <Toxocious@gmail.com>
2021-03-31 01:45:03 -0700

    [Battle] Implement primitive battle AJAX handler
    Currently doesn't really do anything; will be expanded upon once the rest of the foundation is set-up.

---
+++ battles/ajax/handler.php
@@ -0,0 +1,41 @@
+<?php
+  if ( isset($_GET['Battle_ID']) )
+  {
+    require_once '../../core/required/session.php';
+	  require_once '../../battles/classes/battle.php';
+
+    $Fight = $_SESSION['Battle']['Battle_Type'];
+    $Battle = new $Fight();
+
+    if ( $_GET['Battle_ID'] != $_SESSION['Battle']['Battle_ID'] )
+    {
+      $Output = null;
+
+      return;
+    }
+
+    $Output = [
+      'Time_Started' => $_SESSION['Battle']['Time_Started'],
+      'Battle_Type' => $_SESSION['Battle']['Battle_Type'],
+      'Started' => $_SESSION['Battle']['Started'],
+      'Battle_ID' => $_SESSION['Battle']['Battle_ID'],
+      'Message' => [
+        'Type' => 'Success',
+        'Text' => 'Successfully procced battle handler.',
+      ],
+    ];
+
+    foreach ( ['Ally', 'Foe'] as $Side )
+    {
+      $Output[$Side] = $_SESSION['Battle'][$Side];
+    }
+  }
+  else
+  {
+    $Output['Message'] = [
+      'Type' => 'Error',
+      'Text' => 'Your Battle ID is not set.'
+    ];
+  }
+
+  echo json_encode($Output);
