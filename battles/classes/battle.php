ac4854cf1ce953242fefde350dc83476d86db732
Toxocious <Toxocious@gmail.com>
2021-03-31 01:37:42 -0700

    [Battle] Implement foundation for the Battle Class
    Currently quite barren, but will get fleshed out once the other classes are more developed.

---
+++ battles/classes/battle.php
@@ -0,0 +1,48 @@
+<?php
+  namespace BattleHandler;
+  
+  spl_autoload_register(function($Class)
+  {
+    $Battle_Directory = dirname(__DIR__, 1);
+    $Class = strtolower($Class);
+
+    if ( file_exists($Battle_Directory . "\\classes\\{$Class}.php"))
+      require_once $Battle_Directory . "\\classes\\{$Class}.php";
+    
+    if ( file_exists($Battle_Directory . "\\fights\\{$Class}.php"))
+      require_once $Battle_Directory . "\\fights\\{$Class}.php";
+  });
+
+  class Battle
+  {
+    public $Started = false;
+    public $Ended = false;
+
+    public $Battle_Sim = false;
+    public $Battle_Sim_Difficulty = false;
+    
+    public $Battle_Type = null;
+
+    public $Processing_Side = null;
+
+    public $Last_Move = null;
+
+    public $Weather = null;
+    public $Weather_Turns_Left = null;
+
+    public $Time_Started = null;
+    public $Time_Ended = null;
+    public $Layout = null;
+
+    public $Ally = null;
+    public $Foe = null;
+
+    public $Battle_ID = null;
+
+    public $Turn_ID = 0;
+
+    public function __construct()
+    {
+
+    }
+  }
