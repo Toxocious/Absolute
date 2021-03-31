f0748c41191aa7f21ec1f5a99bef30d6966855c0
Toxocious <Toxocious@gmail.com>
2021-03-31 01:46:22 -0700

    [Battle] Update main battle.php page
    Supports multiple battle layouts (toggleable via the user's option page soon). Implemented updated pure JS functions to handle the processing of returned JSON data from the battle handler.

--- battle.php
+++ battle.php
@@ -1,291 +1,88 @@
 <?php
-	require_once 'core/required/layout_top.php';
-	require_once 'battles/battle.php';
+	require_once 'core/required/session.php';
 
-	if ( !isset($_SESSION['Battle']) )
+	switch ($User_Data['Battle_Theme'])
 	{
-		$Error = "
-			<div class='error'>
-				A battle has not yet been created.
-			</div>
-		";
-	}
-	else
-	{
-		$Fight = $_SESSION['Battle']['Status']['Battle_Fight'];
-		$Battle = new $Fight();
+		case 'Default':
+			require_once 'battles/themes/default.php';
+			break;
+		case 'Debug':
+			require_once 'battles/themes/debug.php';
+			break;
+		default:
+			require_once 'battles/themes/default.php';
+			break;
 	}
 ?>
 
-<div class='panel content'>
-	<div class='head'>Battle</div>
-	<div class='body' id='BattleWindow'>
-		<?= ( isset($Error) ? $Error : '' ); ?>
-
-		<div style='margin: 0 auto; width: 75%;'>
-			<div>
-				<!-- Your Pokemon -->
-				<div style='float: left; width: 45%;'>
-					<!-- roster -->
-					<div class='roster'>
-						<div class="slot" id='A_1' style='display: none;'></div>
-						<div class="slot" id='A_2' style='display: none;'></div>
-						<div class="slot" id='A_3' style='display: none;'></div>
-						<div class="slot" id='A_4' style='display: none;'></div>
-						<div class="slot" id='A_5' style='display: none;'></div>
-						<div class="slot" id='A_6' style='display: none;'></div>
-					</div>
-					<!-- roster -->
-
-					<!-- active -->
-					<div class='active'>
-						<div class='sprite'>
-							<img id='A_A' />
-						</div>
-						<div class='name'>
-							<div>
-								<div id='A_A_Name'></div>
-								<div style='font-size: 12px; text-align: left;'>
-									HP: (<span id='A_A_HP_Cur'></span>/<span id='A_A_HP_Max'></span>)
-								</div>
-								<div class='hp_bar'>
-									<span id='A_A_HP'></span>
-								</div>
-								<div style='font-size: 12px; text-align: left;'>
-									Level: <span id='A_A_Level'></span>
-								</div>
-								<div class='exp_bar'>
-									<span id='A_A_EXP'></span>
-								</div>
-							</div>
-						</div>
-					</div>
-					<!-- active -->
-				</div>
-				<!-- Your Pokemon -->
-
-				<!-- Battle Options/Bag -->
-				<div class='battle_options'>
-					<div style='padding-top: 6px;'>
-						<img src='<?= DOMAIN_ROOT; ?>/images/Assets/options.png' style='height: 50px; width: 50px;' />
-					</div>
-				</div>
-				<!-- Battle Options/Bag -->
-
-				<!-- Enemy Pokemon -->
-				<div style='float: left; width: 45%;'>
-					<!-- roster -->
-					<div class='roster'>
-						<div class="slot" id='D_1' style='display: none;'></div>
-						<div class="slot" id='D_2' style='display: none;'></div>
-						<div class="slot" id='D_3' style='display: none;'></div>
-						<div class="slot" id='D_4' style='display: none;'></div>
-						<div class="slot" id='D_5' style='display: none;'></div>
-						<div class="slot" id='D_6' style='display: none;'></div>
-					</div>
-					<!-- roster -->
-
-					<!-- active -->
-					<div class='active'>
-						<div class='sprite'>
-							<img id='D_A' />
-						</div>
-						<div class='name'>
-							<div>
-								<div id='D_A_Name'></div>
-								<div style='font-size: 12px; text-align: left;'>
-									HP: (<span id='D_A_HP_Cur'></span>/<span id='D_A_HP_Max'></span>)
-								</div>
-								<div class='hp_bar'>
-									<span id='D_A_HP'></span>
-								</div>
-								<div style='font-size: 12px; text-align: left;'>
-									Level: <span id='D_A_Level'></span>
-								</div>
-								<div class='exp_bar'>
-									<span id='D_A_EXP'></span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- Enemy Pokemon -->
-			</div>
-
-			<br /><br />
-			<br /><br />
-			<br /><br />
-
-			<!-- Moves -->
-			<div class='battle_moves'>
-				<input id='Move_1' style='padding: 5px; width: 150px;' type='button' />
-				<input id='Move_2' style='padding: 5px; width: 150px;' type='button' />
-				<input id='Move_3' style='padding: 5px; width: 150px;' type='button' />
-				<input id='Move_4' style='padding: 5px; width: 150px;' type='button' />
-			</div>
-			<!-- Moves -->
-
-			<div id='BattleStatus'></div>
-		</div>
-	</div>
-</div>
-
 <script type='text/javascript'>
-	$(function()
-	{
-		Battle.Loading();
+	const Battle = {
+		Loading: false,
+		Ended: false,
+		Bag: false,
+		Clicks: [],
+		ID: null,
 
-		$.ajax({
-			type: 'POST',
-			url: 'battles/handler.php',
-			data: { bid: '<?= $_SESSION['Battle']['Status']['Battle_ID'] ?>' },
-			success: function(JSON)
-			{
-				Battle.Render(JSON);
-			},
-			error: function(JSON)
-			{
-				Battle.Render(JSON);
-			}
-		});
-	});
-
-	/**
-	 * Battle Functions
-	 */
-	Battle = {};
-	Battle.Clicks = 0;
-
-	$(document).on('click', function()
-	{
-		Battle.Clicks++;
-	});
-
-	Battle.Loading = function()
-	{
-		$('#BattleStatus').html("<div style='padding: 10px;'><div class='spinner' style='left: 48.5%; position: relative;'></div></div>");
-	}
-
-	Battle.Render = function(JSON)
-	{
-		console.log(JSON);
-		var Pokemon;
-		var Prefix;
-
-		/**
-		 * Render both user's rosters.
-		 */
-		for ( let i = 0; i < 2; i++ )
+		OnPageLoad: () =>
 		{
-			switch ( i )
+			setTimeout(() =>
 			{
-				case 0:
-					Pokemon = JSON.Attacker.Active;
-					Prefix = "A_";
-					break;
-				case 1:
-					Pokemon = JSON.Defender.Active;
-					Prefix = "D_";
-					break;
-			}
+				Battle.HandleRequest(null, null);
+			}, 250);
+		},
 
-			if ( Pokemon != undefined )
+		RenderRoster: (Side, Roster) =>
+		{
+			for ( let i = 0; i < Roster.length; i++ )
 			{
-				$('#' + Prefix + 'A').attr( 'src', Pokemon['Sprite'] );
-				$('#' + Prefix + 'A_Name').html( `<b>${Pokemon['Name']}</b>` );
-				$('#' + Prefix + 'A_Level').html( Pokemon['Level'] );
-				$('#' + Prefix + 'A_HP_Cur').html( Pokemon['HP']['Current'] );
-				$('#' + Prefix + 'A_HP_Max').html( Pokemon['HP']['Max'] );
+				document.querySelector(`[slot='${Side}_Slot_${i}'] > img`).setAttribute('src', Roster[i].Icon);
 
-				$('#' + Prefix + 'A_HP').attr('title', Pokemon['HP']['Current'] + '/' + Pokemon['HP']['Max']).animate({ 'width': ((Pokemon['HP']['Current'] * 124) / Pokemon['HP']['Max']) + 'px' }, 200);
-				$('#' + Prefix + 'A_EXP').attr('title', Pokemon['Exp']['Current'] + '/' + Pokemon['Exp']['Needed']).animate({ 'width': (Pokemon['Exp']['Current'] / Pokemon['Exp']['Needed']) * 124 + 'px' }, 200);
-			}
-
-			for ( let o = 0; o <= 5; o++ )
-			{
-				if ( Prefix == 'A_' )
+				if ( Roster[i].Active )
 				{
-					var Roster_Pokemon = JSON.Attacker[o];
-				}
-
-				if ( Prefix == 'D_' )
-				{
-					var Roster_Pokemon = JSON.Defender[o];
-				}
-
-				if ( Roster_Pokemon != undefined )
-				{
-					$('#' + Prefix + ( o + 1 )).html( `<div><img src='${Roster_Pokemon['Icon']}' /></div>` ).css({ 'display':'block' });
+					document.querySelector(`[slot='${Side}_Active'] > img`).setAttribute('src', Roster[i].Sprite);
+					document.querySelector(`[slot='${Side}_Name']`).innerHTML = Roster[i].Display_Name;
+					document.querySelector(`[slot='${Side}_HP']`).innerHTML = Roster[i].HP;
+					document.querySelector(`[slot='${Side}_Max_HP']`).innerHTML = Roster[i].Max_HP;
+					document.querySelector(`[slot='${Side}_Level']`).innerHTML = Roster[i].Level.toLocaleString();
 				}
 			}
-		}
+		},
 
-		/**
-		 * Render the user's moves.
-		 */
-		$('#Move_1').attr('value', JSON.Attacker.Active.Moves.Move_1.Move_Name).attr('Postcode', JSON.Attacker.Active.Moves.Move_1.Postcode);
-		$('#Move_2').attr('value', JSON.Attacker.Active.Moves.Move_2.Move_Name).attr('Postcode', JSON.Attacker.Active.Moves.Move_2.Postcode);
-		$('#Move_3').attr('value', JSON.Attacker.Active.Moves.Move_3.Move_Name).attr('Postcode', JSON.Attacker.Active.Moves.Move_3.Postcode);
-		$('#Move_4').attr('value', JSON.Attacker.Active.Moves.Move_4.Move_Name).attr('Postcode', JSON.Attacker.Active.Moves.Move_4.Postcode);
-
-		//Battle.Render_Inputs();
-
-		/**
-		 * Render the battle dialogue.
-		 */
-		$('#BattleStatus').html(JSON.Battle.Text);
-	}
-
-	$('.battle_moves input').on('pointerdown', function(e)
-	{
-		Battle.Move( $(this).attr('Postcode'), e );
-		return false;
-	});
-
-	/**
-	 * Render the user's moves.
-	 */
-	Battle.Render_Inputs = function(JSON)
-	{
-		
-	}
-
-	/**
-	 * Handle attacking.
-	 */
-	Battle.Move = function(move, e)
-	{
-		let Element = {
-			'PostCode': $(e.target).attr('postcode'),
-			'Position': $(e.target).position(),
-			'Width'		: parseInt( $(e.target).css('width') ),
-			'Height'	: parseInt( $(e.target).css('height' ) ),
-		};
-
-		$.ajax({
-			type: 'POST',
-			url: 'battles/handler.php',
-			data: {
-				Battle_ID: '<?= $_SESSION['Battle']['Status']['Battle_ID'] ?>',
-				Element: Element,
-				Clicks: Battle.Clicks,
-				Move: move,
-				x: e.pageX,
-				y: e.pageY,
-			},
-			success: function(JSON)
+		HandleRequest: (Data, Callback) =>
+		{
+			if ( !this.Loading )
 			{
-				Battle.Render(JSON);
-				Battle.Clicks = 0;
-			},
-			error: function(JSON)
-			{
-				Battle.Render(JSON);
-				Battle.Clicks = 0;
+				this.ID = '<?= $_SESSION['Battle']['Battle_ID']; ?>';
+
+				return new Promise((resolve, reject) =>
+				{
+					const req = new XMLHttpRequest();
+					req.open('GET', `<?= DOMAIN_ROOT; ?>/battles/ajax/handler.php?Battle_ID=${this.ID}`);
+					req.send();
+					req.onerror = (error) => reject(Error(`Network Error: ${error}`));
+					req.onload = () =>
+					{
+						let JSON_Data = JSON.parse(req.response);
+						console.log(JSON_Data);
+
+						if ( req.status === 200 )
+						{
+							Battle.RenderRoster('Ally', JSON_Data.Ally.Roster);
+							Battle.RenderRoster('Foe', JSON_Data.Foe.Roster);
+
+							document.getElementById('BattleDialogue').innerHTML = JSON_Data.Message.Text;
+							resolve(req.response);
+						}
+						else
+						{
+							document.getElementById('BattleDialogue').innerHTML = JSON_Data.Message.Text;
+							reject(Error(req.statusText))
+						}
+					};
+				});
 			}
-		});
-	}
+		},
+	};
+	
+	window.onload = Battle.OnPageLoad();
 </script>
-
-<?php
-	require_once 'core/required/layout_bottom.php';
