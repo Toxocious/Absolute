ca5576956a3cf5f60c30a9bad916931a39de879f
Toxocious <Toxocious@gmail.com>
2021-03-31 01:38:38 -0700

    [Battle] Implement Status Class foundation

---
+++ battles/classes/status.php
@@ -0,0 +1,15 @@
+<?php
+  use BattleHandler\Battle;
+  
+  class Status
+  {
+    public $Pokemon_ID = null;
+
+    public $Status = null;
+    public $Status_Stage = null;
+
+    public function __construct()
+    {
+
+    }
+  }
