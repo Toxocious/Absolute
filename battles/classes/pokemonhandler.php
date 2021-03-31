0b092639ec117a60c5ab328b201953fdc919a6f2
Toxocious <Toxocious@gmail.com>
2021-03-31 01:42:32 -0700

    [Battle] Implement PokemonHandler Class to handle Pokemon
    Contains all necessary information that the battle could possibly require. Will be expanded upon to further support specific handling of Pokemon, such as fainting, gaining exp/evs/ivs, etc.

---
+++ battles/classes/pokemonhandler.php
@@ -0,0 +1,97 @@
+<?php
+  use BattleHandler\Battle;
+
+  class PokemonHandler
+  {
+    public $Pokemon_ID = null;
+    
+    public $Display_Name = null;
+
+    public $Side = null;
+    public $Active = null;
+    public $Slot = null;
+    
+    public $Shiny = null;
+    public $Gender = null;
+    public $Level = null;
+
+    public $HP = null;
+    public $Max_HP = null;
+
+    public $Base_Stats = null;
+    public $Current_Stats = null;
+    public $IVs = null;
+    public $EVs = null;
+
+    public $Moves = null;
+    public $Ability = null;
+
+    public $Item = null;
+
+    public $Status = null;
+    public $Status_Stage = null;
+    
+    public $Last_Move = null;
+
+    public $Sprite = null;
+    public $Icon = null;
+
+    public $Fainted = false;
+
+    public function __construct
+    (
+      $Pokemon_ID,
+      $Side,
+      $Slot
+    )
+    {
+      $this->Pokemon_ID = $Pokemon_ID;
+      $this->Side = $Side;
+      $this->Slot = $Slot;
+      $this->Active = ($Slot == 1 ? true : false);
+      
+      $this->Fainted = false;
+
+      $this->ProcessPokemon();
+    }
+
+    public function ProcessPokemon()
+    {
+      global $Poke_Class;
+
+      $Pokemon = $Poke_Class->FetchPokemonData($this->Pokemon_ID);
+      if ( !$Pokemon )
+        return false;
+
+      $this->Sprite = $Pokemon['Sprite'];
+      $this->Icon = $Pokemon['Icon'];
+      $this->Display_Name = $Pokemon['Display_Name'];
+      $this->Shiny = ($Pokemon['Type'] == 'Shiny' ? true : false);
+      // $this->Ability = ($Pokemon['Ability'] ? $Pokemon['Ability'] : null); // abilities are currently not in game in any capacity
+      $this->Gender = $Pokemon['Gender'];
+      $this->Level = $Pokemon['Level_Raw'];
+      $this->HP = $Pokemon['Stats'][0];
+      $this->Max_HP = $Pokemon['Stats'][0];
+      $this->Current_Stats['Attack'] = $Pokemon['Stats'][1];
+      $this->Current_Stats['Defense'] = $Pokemon['Stats'][2];
+      $this->Current_Stats['SpAttack'] = $Pokemon['Stats'][3];
+      $this->Current_Stats['SpDefense'] = $Pokemon['Stats'][4];
+      $this->Current_Stats['Speed'] = $Pokemon['Stats'][5];
+      $this->Base_Stats['Attack'] = $Pokemon['BaseStats'][1];
+      $this->Base_Stats['Defense'] = $Pokemon['BaseStats'][2];
+      $this->Base_Stats['SpAttack'] = $Pokemon['BaseStats'][3];
+      $this->Base_Stats['SpDefense'] = $Pokemon['BaseStats'][4];
+      $this->Base_Stats['Speed'] = $Pokemon['BaseStats'][5];
+      $this->IVs = $Pokemon['IVs'];
+      $this->EVs = $Pokemon['EVs'];
+      $this->Moves = [
+        new Move($Pokemon['Move_1']),
+        new Move($Pokemon['Move_2']),
+        new Move($Pokemon['Move_3']),
+        new Move($Pokemon['Move_4']),
+      ];
+      $this->Item = new HeldItem($Pokemon['Item_ID']);
+
+      return $this;
+    }
+  }
