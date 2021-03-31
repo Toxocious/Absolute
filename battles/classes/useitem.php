e8ec715215ca13b48b852697de517d4f3832ccb0
Toxocious <Toxocious@gmail.com>
2021-03-31 01:38:26 -0700

    [Battle] Implement UseItem Class foundation

---
+++ battles/classes/useitem.php
@@ -0,0 +1,17 @@
+<?php
+  use BattleHandler\Battle;
+  
+  class UseItem
+  {
+    public $Pokemon_ID = null;
+
+    public $Item = null;
+    public $Item_Effect = null;
+
+    public $Success = false;
+
+    public function __construct()
+    {
+      
+    }
+  }
