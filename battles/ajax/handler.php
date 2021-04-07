f3aaeaba5058a17df66afdf872f6e2dabfcf08d7
Toxocious <Toxocious@gmail.com>
2021-04-06 17:19:07 -0700

    [Battle] Implement logic for switching Pokemon

--- battles/ajax/handler.php
+++ battles/ajax/handler.php
@@ -19,11 +19,65 @@
       'Battle_Type' => $_SESSION['Battle']['Battle_Type'],
       'Started' => $_SESSION['Battle']['Started'],
       'Battle_ID' => $_SESSION['Battle']['Battle_ID'],
-      'Message' => [
-        'Type' => 'Success',
-        'Text' => 'Successfully procced battle handler.',
-      ],
+      'Turn_ID' => $_SESSION['Battle']['Turn_ID'],
     ];
+
+    /**
+     * Process the desired battle action.
+     */
+    if ( isset($_GET['Action']) )
+    {
+      $Action = Purify($_GET['Action']);
+
+      switch ($Action)
+      {
+        case 'Switch':
+          if ( !isset($_GET['Slot']) )
+          {
+            $Output['Message'] = [
+              'Type' => 'Error',
+              'Text' => 'An error occurred while switching your active Pok&eacute;mon.'
+            ];
+          }
+          else
+          {
+            $Slot = Purify($_GET['Slot']) - 1;
+
+            if ( $_SESSION['Battle']['Ally']['Active']->Pokemon_ID != $_SESSION['Battle']['Ally']['Roster'][$Slot]->Pokemon_ID )
+            {
+              if ( $_SESSION['Battle']['Ally']['Roster'][$Slot]->HP <= 0 )
+              {
+                $Output['Message'] = [
+                  'Type' => 'Error',
+                  'Text' => 'You can not swap into a fainted Pok&eacute;mon.'
+                ];
+              }
+              else
+              {
+                $Output['Message'] = $_SESSION['Battle']['Ally']['Roster'][$Slot]->SwitchInto();
+              }
+            }
+            else
+            {
+              $Output['Message'] = [
+                'Type' => 'Error',
+                'Text' => 'The Pok&eacute;mon that you\'re switching into is already out!'
+              ];
+            }
+          }
+          break;
+
+        default:
+          break;
+      }
+    }
+    else
+    {
+      $Output['Message'] = [
+        'Type' => 'Success',
+        'Text' => 'The battle has begun.'
+      ];
+    }
 
     foreach ( ['Ally', 'Foe'] as $Side )
     {
