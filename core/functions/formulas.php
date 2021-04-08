67abef6429283e1864b277c6d99d660cad9a662b
Toxocious <Toxocious@gmail.com>
2021-04-07 19:35:48 -0700

    [Formulas] Implement FetchExpToNextLevel formula

--- core/functions/formulas.php
+++ core/functions/formulas.php
@@ -3,7 +3,7 @@
 	 * FORMULAS FOR EACH EXP GROUP ( ^ meaning to the power of, ie level ^ 3 = level cubed, level ^ 2 = level squared )
 	 * =============================
 	 * Fast:       (level)^3 * 0.8
-	 * Medium:     (level)^3 
+	 * Medium:     (level)^3
 	 * Parabolic:  (level)^3 * 1.2 - (level)^2 * 15 - (level) * 100 - 140
 	 * Slow:       (level)^3 * 1.25
 	 */
@@ -49,7 +49,40 @@
 				return pow($Level, 3);
 			case 'Clan':
 				return pow($Level, 2.2);
-			default: 
+			default:
 				return false;
 		}
 	}
+
+  /**
+   * Calculate how much Exp. is needed to reach the next level.
+   * @param int $Current_Exp
+   * @param string $Formula
+   */
+  function FetchExpToNextLevel($Current_Exp, $Formula, $Return_Percent = false)
+  {
+    $Current_Level = FetchLevel($Current_Exp, $Formula);
+    $Next_Level_Exp = FetchExperience($Current_Level + 1, $Formula);
+
+    if (!$Return_Percent)
+    {
+      return $Next_Level_Exp - $Current_Exp;
+    }
+    else
+    {
+      $Last_Level_Exp = FetchExperience($Current_Level, $Formula);
+
+      if ($Current_Exp == 0)
+      {
+        return [
+          'Percent' => 0,
+          'Exp' => 1,
+        ];
+      }
+
+      return [
+        'Percent' => round(100 * (1 - (($Next_Level_Exp - $Current_Exp) / ($Next_Level_Exp - $Last_Level_Exp))), 2),
+        'Exp' => $Next_Level_Exp - $Current_Exp,
+      ];
+    }
+  }
