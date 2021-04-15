35c9b474270ec6593ca01802e8b08258bdd37f70
Toxocious <Toxocious@gmail.com>
2021-04-14 21:38:49 -0700

    [Battle] Update Move attributes on render

--- battle.php
+++ battle.php
@@ -116,7 +116,8 @@
 
       for ( let i = 0; i < Moves.length; i++ )
       {
-        document.querySelector(`[move='Move_${i}']`).setAttribute('class', Moves[i].Type);
+        document.querySelector(`[move='Move_${i}']`).setAttribute('onclick', `Battle.Move(${Moves[i].ID})`);
+        document.querySelector(`[move='Move_${i}']`).setAttribute('class', Moves[i].Move_Type);
         document.querySelector(`[move='Move_${i}']`).value = Moves[i].Name;
       }
     },
