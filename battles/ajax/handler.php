13d7df5fee8b83bb95c4bbd09a5df200c07340c3
Toxocious <Toxocious@gmail.com>
2021-06-17 18:02:52 -0700

    [Battle] Update handler.php logic
    Just a slight update so that the file is a bit cleaner.

--- battles/ajax/handler.php
+++ battles/ajax/handler.php
@@ -1,64 +1,62 @@
 <?php
-  if ( isset($_GET['Battle_ID']) )
-  {
-    require_once '../../battles/classes/battle.php';
-    require_once '../../core/required/session.php';
-
-    $Fight = $_SESSION['Battle']['Battle_Type'];
-    $Battle = new $Fight();
-
-    if ( $_GET['Battle_ID'] != $_SESSION['Battle']['Battle_ID'] )
-    {
-      $Output = null;
-
-      return;
-    }
-
-    $Output = [
-      'Time_Started' => $_SESSION['Battle']['Time_Started'],
-      'Battle_Type' => $_SESSION['Battle']['Battle_Type'],
-      'Started' => $_SESSION['Battle']['Started'],
-      'Battle_ID' => $_SESSION['Battle']['Battle_ID'],
-      'Turn_ID' => $_SESSION['Battle']['Turn_ID'],
-    ];
-
-    /**
-     * Process the desired battle action.
-     */
-    if ( isset($_GET['Action']) )
-    {
-      $Action = Purify($_GET['Action']);
-
-      $Turn_Data = $Battle->ProcessTurn($Action);
-
-      $Output['Message'] = $Turn_Data;
-    }
-    else
-    {
-      if ( isset($_SESSION['Battle']['Dialogue']) )
-      {
-        $Output['Message'] = $_SESSION['Battle']['Dialogue'];
-      }
-      else
-      {
-        $Output['Message'] = [
-          'Type' => 'Success',
-          'Text' => 'The battle has begun.'
-        ];
-      }
-    }
-
-    foreach ( ['Ally', 'Foe'] as $Side )
-    {
-      $Output[$Side] = $_SESSION['Battle'][$Side];
-    }
-  }
-  else
+  if ( !isset($_GET['Battle_ID']) )
   {
     $Output['Message'] = [
       'Type' => 'Error',
       'Text' => 'Your Battle ID is not set.'
     ];
+  }
+
+  require_once '../../battles/classes/battle.php';
+  require_once '../../core/required/session.php';
+
+  $Fight = $_SESSION['Battle']['Battle_Type'];
+  $Battle = new $Fight();
+
+  if ( $_GET['Battle_ID'] != $_SESSION['Battle']['Battle_ID'] )
+  {
+    $Output = null;
+
+    return;
+  }
+
+  $Output = [
+    'Time_Started' => $_SESSION['Battle']['Time_Started'],
+    'Battle_Type' => $_SESSION['Battle']['Battle_Type'],
+    'Started' => $_SESSION['Battle']['Started'],
+    'Battle_ID' => $_SESSION['Battle']['Battle_ID'],
+    'Turn_ID' => $_SESSION['Battle']['Turn_ID'],
+  ];
+
+  /**
+   * Process the desired battle action.
+   */
+  if ( isset($_GET['Action']) )
+  {
+    $Action = Purify($_GET['Action']);
+
+    $Turn_Data = $Battle->ProcessTurn($Action);
+
+    $Output['Message'] = $Turn_Data;
+  }
+  else
+  {
+    if ( isset($_SESSION['Battle']['Dialogue']) )
+    {
+      $Output['Message'] = $_SESSION['Battle']['Dialogue'];
+    }
+    else
+    {
+      $Output['Message'] = [
+        'Type' => 'Success',
+        'Text' => 'The battle has begun.'
+      ];
+    }
+  }
+
+  foreach ( ['Ally', 'Foe'] as $Side )
+  {
+    $Output[$Side] = $_SESSION['Battle'][$Side];
   }
 
   $_SESSION['Battle']['Dialogue'] = $Output['Message'];
