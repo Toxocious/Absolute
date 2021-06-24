65ccb674fb43c18e044ae1aa4352e721092910e4
Toxocious <Toxocious@gmail.com>
2021-06-23 18:15:09 -0700

    [Battle] Remove duplicate Taunt check
    ??? I'm so confused.

--- battles/classes/move.php
+++ battles/classes/move.php
@@ -154,19 +154,6 @@
         }
       }
 
-      if ( $Attacker->HasStatus('Taunt') )
-      {
-        if ( $this->Damage_Type == 'Status' )
-        {
-          return [
-            'Type' => 'Error',
-            'Text' => "{$Attacker->Display_Name} can't use {$this->Name} due to the Taunt.",
-            'Damage' => 0,
-            'Heal' => 0,
-          ];
-        }
-      }
-
       $Attacker_Can_Move = $this->CanUserMove($Side);
       if ( $Attacker_Can_Move['Type'] == 'Error' )
       {
