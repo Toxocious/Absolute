637f4269766ee6175f5f938293c713b03cd99923
Toxocious <Toxocious@gmail.com>
2021-07-15 22:54:57 -0700

    [Battle] Implement Rewards->CalcMoneyYield()
    Determines how much Money the user will be awarded for winning the battle.

--- battles/classes/rewards.php
+++ battles/classes/rewards.php
@@ -5,4 +5,24 @@
   {
     public $Money = 0;
     public $Abso_Coins = 0;
+
+    /**
+     * Calculate how much money the user is awarded.
+     */
+    public function CalcMoneyYield()
+    {
+      $Money = $this->Money;
+
+      foreach ( $_SESSION['Battle']['Foe']->Roster as $Pokemon )
+        $Money += $Pokemon->Level;
+
+      if ( isset($_SESSION['Battle']['Pay_Day']) )
+        $Money += $_SESSION['Battle']['Pay_Day'];
+
+      $Clan_Bonus = $_SESSION['Battle']['Ally']->Clan->HasUpgrade(4);
+      if ( $Clan_Bonus )
+        $Money *= floor(100 / $Clan_Bonus['Current_Level']);
+
+      return $Money;
+    }
   }
