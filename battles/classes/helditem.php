89603102079cde2825b33c9520e4b2da36a9db48
Toxocious <Toxocious@gmail.com>
2021-07-24 16:57:48 -0700

    [Battle] Fetch Held Item From DB Table `items`
    Was fetching from the `item_dex` table, which shouldn't happen.

--- battles/classes/helditem.php
+++ battles/classes/helditem.php
@@ -2,11 +2,10 @@
   class HeldItem
   {
     public $ID = null;
+    public $Item_ID = null;
     public $Name = null;
-    public $Effect = null;
-    public $Description = null;
     public $Uses_Left = null;
-    public $Has_Procced = false;
+    public $Owner_Current = null;
 
     public function __construct
     (
@@ -17,7 +16,12 @@
 
       try
       {
-        $Fetch_Item = $PDO->prepare("SELECT * FROM `item_dex` WHERE `Item_ID` = ? LIMIT 1");
+        $Fetch_Item = $PDO->prepare("
+          SELECT `id`, `Item_ID`, `Item_Name`, `Owner_Current`, `Quantity`
+          FROM `items`
+          WHERE `Item_ID` = ?
+          LIMIT 1
+        ");
         $Fetch_Item->execute([ $Item_ID ]);
         $Fetch_Item->setFetchMode(PDO::FETCH_ASSOC);
         $Item_Data = $Fetch_Item->fetch();
@@ -30,10 +34,10 @@
       if ( !$Item_Data )
         return false;
 
-      $this->ID = $Item_Data['Item_ID'];
+      $this->ID = $Item_Data['id'];
+      $this->Item_ID = $Item_Data['Item_ID'];
       $this->Name = $Item_Data['Item_Name'];
-      $this->Description = $Item_Data['Item_Description'];
-      $this->Effect = null;
+      $this->Owner_Current = $Item_Data['Owner_Current'];
       $this->Uses_Left = -1;
 
       return $this;
