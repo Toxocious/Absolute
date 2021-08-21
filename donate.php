dc12049f1a0042fe20d012f335049a74f0b89d3b
Jesse Mack <Toxocious@gmail.com>
2021-08-21 03:35:29 -0700

    [Donate] Update Donate w/ Coincase Commerce links

--- donate.php
+++ donate.php
@@ -8,46 +8,130 @@
     <div class='description'>
       If you would like to support Absolute's development, please consider donating below.
       <br />
-      We currently accept Bitcoin and Ethereum.
+      We currently accept a wide variety of various crypto-currencies.
       <br /><br />
-      <b>We will need the <u>transaction ID</u> of your Bitcoin/Ethereum transaction to confirm your donation.</b>
+      Payments are automatically handled.
+      <br />
+      Once confirmed, chat will be notified, and your account will be upgraded automatically.
     </div>
 
     <div class='flex wrap' style='justify-content: center;'>
-      <div class='border-gradient' style='flex-basis: 40%; margin: 0px 5px;'>
-        <div>
-          <img src='<?= DOMAIN_SPRITES . '/Assets/btc-svg.png' ?>' style='height: 100px; width: 100px;' />
-          <br />
-          <b>Bitcoin</b>
-          <br /><br />
+      <table class='border-gradient' style='flex-basis: 250px;'>
+        <tbody>
+          <tr>
+            <td>
+              <h2>Absolute Premium</h2>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h2>$5.00</h2>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h3>Benefits</h3>
 
-          <form name='btc_form'>
-            How much would you like to donate?<br />
-            <i>This value is presumed to be USD.</i>
+              &bull; Perk<br />
+              &bull; Perk<br />
+              &bull; Perk<br />
+              &bull; Perk<br />
+              &bull; Perk<br />
+              &bull; Perk<br />
+              &bull; Perk<br />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <a
+                class='buy-with-crypto'
+                href='https://commerce.coinbase.com/checkout/17344145-77af-4d01-bdb2-e8688e68a55b'
+              >
+                Buy with Crypto
+              </a>
+              <script src="https://commerce.coinbase.com/v1/checkout.js?version=201807"></script>
+            </td>
+          </tr>
+        </tbody>
+      </table>
 
-            <div class='flex'>
+      <table class='border-gradient' style='flex-basis: 250px;'>
+        <tbody>
+          <tr>
+            <td>
+              <h2>Absolute Exclusive</h2>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h2>$10.00</h2>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h3>Benefits</h3>
 
-            </div>
-          </form>
-        </div>
-      </div>
+              &bull; Perk<br />
+              &bull; Perk<br />
+              &bull; Perk<br />
+              &bull; Perk<br />
+              &bull; Perk<br />
+              &bull; Perk<br />
+              &bull; Perk<br />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <a
+                class='buy-with-crypto'
+                href='https://commerce.coinbase.com/checkout/b4be1fa5-678f-4c99-a74d-c48338341fc8'
+              >
+                Buy with Crypto
+              </a>
+              <script src="https://commerce.coinbase.com/v1/checkout.js?version=201807"></script>
+            </td>
+          </tr>
+        </tbody>
+      </table>
 
-      <div class='border-gradient' style='flex-basis: 40%; margin: 0px 5px;'>
-        <div>
-          <img src='<?= DOMAIN_SPRITES . '/Assets/eth-svg.png' ?>' style='height: 100px; width: 100px;' />
-          <br />
-          <b>Ethereum</b>
-          <br /><br />
+      <table class='border-gradient' style='flex-basis: 250px;'>
+        <tbody>
+          <tr>
+            <td>
+              <h2>Absolute Luxurious</h2>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h2>$20.00</h2>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h3>Benefits</h3>
 
-          <form name='eth_form'>
-            How much would you like to donate?<br />
-            <i>This value is presumed to be USD.</i>
-            <select>
-              <option></option>
-            </select>
-          </form>
-        </div>
-      </div>
+              &bull; Perk<br />
+              &bull; Perk<br />
+              &bull; Perk<br />
+              &bull; Perk<br />
+              &bull; Perk<br />
+              &bull; Perk<br />
+              &bull; Perk<br />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <a
+                class='buy-with-crypto'
+                href='https://commerce.coinbase.com/checkout/98e86d93-45ab-47d1-8693-ee8d831da5f2'
+              >
+                Buy with Crypto
+              </a>
+              <script src="https://commerce.coinbase.com/v1/checkout.js?version=201807"></script>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
 	</div>
 </div>
