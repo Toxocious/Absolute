a3a522e2d8ceec4c8ec1109c75cadc4ca3505e94
Jesse Mack <Toxocious@gmail.com>
2021-09-16 17:26:21 -0700

    [Battle] Fetch additional held-item data

--- battles/classes/helditem.php
+++ battles/classes/helditem.php
@@ -34,12 +34,40 @@
       if ( !$Item_Data )
         return false;
 
+      try
+      {
+        $Fetch_Item_Data = $PDO->prepare("
+          SELECT `Natural_Gift_Power`, `Natural_Gift_Type`, `Fling_Power`, `Mega_Evolves`, `Attack_Boost`, `Defense_Boost`, `Sp_Attack_Boost`, `Sp_Defense_Boost`, `Speed_Boost`, `Accuracy_Boost`, `Evasion_Boost`
+          FROM `item_dex`
+          WHERE `Item_ID` = ?
+          LIMIT 1
+        ");
+        $Fetch_Item_Data->execute([ $Item_ID ]);
+        $Fetch_Item_Data->setFetchMode(PDO::FETCH_ASSOC);
+        $Item_Dex_Data = $Fetch_Item_Data->fetch();
+      }
+      catch ( PDOException $e )
+      {
+        HandleError($e);
+      }
+
       $this->ID = $Item_Data['id'];
       $this->Item_ID = $Item_Data['Item_ID'];
       $this->Name = $Item_Data['Item_Name'];
       $this->Owner_Current = $Item_Data['Owner_Current'];
       $this->Uses_Left = -1;
 
+      $this->Natural_Gift_Power = $Item_Dex_Data['Natural_Gift_Power'];
+      $this->Natural_Gift_Type = $Item_Dex_Data['Natural_Gift_Type'];
+      $this->Fling_Power = $Item_Dex_Data['Fling_Power'];
+      $this->Mega_Evolvs = $Item_Dex_Data['Mega_Evolves'];
+      $this->Attack_Boost = $Item_Dex_Data['Attack_Boost'];
+      $this->Defense_Boost = $Item_Dex_Data['Defense_Boost'];
+      $this->Sp_Attack_Boost = $Item_Dex_Data['Sp_Attack_Boost'];
+      $this->Sp_Defense_Boost = $Item_Dex_Data['Sp_Defense_Boost'];
+      $this->Speed_Boost = $Item_Dex_Data['Speed_Boost'];
+      $this->Accuracy_Boost = $Item_Dex_Data['Accuracy_Boost'];
+      $this->Evasion_Boost = $Item_Dex_Data['Evasion_Boost'];
       return $this;
     }
 
