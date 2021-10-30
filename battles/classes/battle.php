fdb118992ae168187639c0e084206a81a0036d7e
Jess <toxocious@gmail.com>
2021-10-29 22:48:53 -0700

    [Battle] Implement 'Lemonade' Use Item
    Poggers, my people.

--- battles/classes/battle.php
+++ battles/classes/battle.php
@@ -1206,6 +1206,15 @@
           ];
           break;
 
+        case 'Lemonade':
+          if ( $Item_Target->HP < $Item_Target->Max_HP )
+          {
+            $HP_Change = [
+              ['Target' => $Item_Target->Slot, 'Amount' => 80]
+            ];
+          }
+          break;
+
         case 'Dire Hit':
           $Item_Target->Critical_Hit_Boost += 1;
           $Use_Item_Dialogue .= "{$Item_Target->Display_Name}'s Critical Hit ratio was boosted!";
