75dce803a8546c3ce5b26748a43443373158cb3b
Jess <toxocious@gmail.com>
2021-11-15 23:01:43 -0800

    [Maps] Render the Player entity at the correct tile

--- js/maps/entities/player.js
+++ js/maps/entities/player.js
@@ -2,7 +2,7 @@ class Player_Entity extends Phaser.Physics.Arcade.Sprite
 {
   constructor(scene, x, y)
   {
-    super(scene, x, y, 'texture');
+    super(scene, x * 16, y * 16, 'texture');
 
     this.scene.add.existing(this);
     this.setTexture('character');
