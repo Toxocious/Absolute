5d2f330134c4a2caea900ddef5df9ba1bd1839d2
Jess <toxocious@gmail.com>
2021-12-01 20:20:56 -0800

    [Maps] Pass object type to NPC entity object

--- maps/js/entities/npc.js
+++ maps/js/entities/npc.js
@@ -1,10 +1,11 @@
 class NPC
 {
-  constructor(Grid_Engine_ID, Sprite, Properties)
+  constructor(Grid_Engine_ID, Sprite, Properties, Type)
   {
     this.Grid_Engine_ID = Grid_Engine_ID;
     this.Sprite = Sprite;
     this.properties = Properties;
+    this.type = Type;
   }
 
   /**
