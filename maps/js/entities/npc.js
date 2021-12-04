53aad356e4ff172b69232c0a30e8f391c95c6e1a
Jess <toxocious@gmail.com>
2021-12-04 07:59:07 -0800

    [Maps] Implement NPC method DisplayDialogue and call it on interation

--- maps/js/entities/npc.js
+++ maps/js/entities/npc.js
@@ -1,7 +1,9 @@
-class NPC
+class NPC extends Phaser.Scene
 {
   constructor(Name, Grid_Engine_ID, Sprite, Properties, Type, Coords)
   {
+    super();
+
     this.Name = Name;
     this.Grid_Engine_ID = Grid_Engine_ID;
     this.Sprite = Sprite;
@@ -16,6 +18,19 @@ class NPC
   Interact()
   {
     console.log(`[NPC | Interaction] Interacting with ${this.Name}`);
+
+    this.GetDialogue();
+    if ( this.Dialogue.length > 0 )
+      this.DisplayDialogue();
+  }
+
+  /**
+   * Display the NPC's dialogue.
+   */
+  DisplayDialogue()
+  {
+    console.log(`[NPC | Dialogue] Displaying the dialogue of ${this.Name}`);
+  }
 
   /**
    * Get all lines of dialogue from the NPC.
