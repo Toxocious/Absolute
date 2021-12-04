190b4a0c90b66d95cb24606c559dde8649535d19
Jess <toxocious@gmail.com>
2021-12-03 16:14:31 -0800

    [Maps] Fix improper indentation

--- maps/js/network.js
+++ maps/js/network.js
@@ -8,48 +8,48 @@ class Network
   /**
    * Send a request to the map handler.
    */
-   SendRequest(Data, HTTP_TYPE = 'GET')
-   {
-     return new Promise((resolve, reject) =>
-     {
-        const req = new XMLHttpRequest();
+  SendRequest(Data, HTTP_TYPE = 'GET')
+  {
+    return new Promise((resolve, reject) =>
+    {
+      const req = new XMLHttpRequest();
 
-        switch ( HTTP_TYPE )
-        {
-          case 'GET':
-            req.open('GET', `${this.AJAX_URL}?${Data}`);
-            req.send();
-            break;
+      switch ( HTTP_TYPE )
+      {
+        case 'GET':
+          req.open('GET', `${this.AJAX_URL}?${Data}`);
+          req.send();
+          break;
 
-          case 'POST':
-            const Data_Val = new FormData();
-            Data_Val.append('Action', Data.Action);
+        case 'POST':
+          const Data_Val = new FormData();
+          Data_Val.append('Action', Data.Action);
 
-            if ( typeof Data.x !== 'undefined' )
-              Data_Val.append('x', Data.x);
-            if ( typeof Data.y !== 'undefined' )
-              Data_Val.append('y', Data.y);
-            if ( typeof Data.z !== 'undefined' )
-              Data_Val.append('z', Data.z);
+          if ( typeof Data.x !== 'undefined' )
+            Data_Val.append('x', Data.x);
+          if ( typeof Data.y !== 'undefined' )
+            Data_Val.append('y', Data.y);
+          if ( typeof Data.z !== 'undefined' )
+            Data_Val.append('z', Data.z);
 
-            req.open('POST', this.AJAX_URL);
-            req.send(Data_Val);
-            break;
+          req.open('POST', this.AJAX_URL);
+          req.send(Data_Val);
+          break;
 
-          default:
-            req.open('GET', `${this.AJAX_URL}?${Data}`);
-            req.send();
-            break;
-        }
+        default:
+          req.open('GET', `${this.AJAX_URL}?${Data}`);
+          req.send();
+          break;
+      }
 
-        req.onerror = (error) => reject(Error(`Network Error: ${error}`));
-        req.onload = () =>
-        {
-          if ( req.status === 200 )
-            resolve(req.response);
-          else
-            reject(Error(req.statusText));
-        };
-     });
-   }
+      req.onerror = (error) => reject(Error(`Network Error: ${error}`));
+      req.onload = () =>
+      {
+        if ( req.status === 200 )
+          resolve(req.response);
+        else
+          reject(Error(req.statusText));
+      };
+    });
+  }
 }
