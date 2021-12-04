585fc8f991e7c6249d35ba4e06e8c4911251683f
Jess <toxocious@gmail.com>
2021-12-03 16:55:23 -0800

    [Maps] Implement Tile method to check obj prop by nane

--- maps/js/tile.js
+++ maps/js/tile.js
@@ -6,4 +6,21 @@ class Tile
     this.y = y;
     this.z = z;
   }
+
+  /**
+   * Check if the tile object has a given property.
+   */
+  DoesObjectHavePropertyOfName(Obj, Property_Name)
+  {
+    if ( typeof Obj !== 'object' || typeof Property_Name !== 'string' )
+      return false;
+
+    for ( const Prop of Obj.properties )
+    {
+      if ( Prop.name == Property_Name )
+        return Prop;
+    }
+
+    return false;
+  }
 }
