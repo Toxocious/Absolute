20d3dd8ad2dbcf056a46cf241c789481796bd058
Jess <toxocious@gmail.com>
2021-12-08 22:29:31 -0800

    [Maps] Implement PHP 'MapObject' Class

---
+++ maps/classes/mapobject.php
@@ -0,0 +1,61 @@
+<?php
+  class MapObject extends Map
+  {
+    /**
+     * Get the object at a given tile coordinate.
+     *
+     * @param {array} $Map_Objects
+     * @param {int} $x
+     * @param {int} $y
+     * @param {int} $z
+     */
+    public static function GetObjectAtTile
+    (
+      array $Map_Objects = null,
+      int $x = null,
+      int $y = null,
+      int $z = null
+    )
+    {
+      if ( empty($Map_Objects) )
+        return false;
+
+      foreach ( $Map_Objects as $Map_Object )
+      {
+        $Get_Layer_Property = self::CheckPropertyByName($Map_Object->properties, 'charLayer');
+        if ( empty($Get_Layer_Property) )
+          return false;
+
+        if
+        (
+          $Map_Object->x / 16 == $x &&
+          $Map_Object->y / 16 == $y &&
+          $Get_Layer_Property->value == "Layer_{$z}"
+        )
+          return true;
+      }
+
+      return false;
+    }
+
+    /**
+     * Check if the object has a desired property.
+     */
+    public static function CheckPropertyByName
+    (
+      array $Properties,
+      string $Property_Name
+    )
+    {
+      if ( empty($Properties) || empty($Property_Name) )
+        return false;
+
+      foreach ( $Properties as $Property )
+      {
+        if ( $Property->name == $Property_Name )
+          return $Property;
+      }
+
+      return false;
+    }
+  }
