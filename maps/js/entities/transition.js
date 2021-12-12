52eac75d0a2a262a1b000be385806cccfbb2ed02
Jess <toxocious@gmail.com>
2021-12-11 19:04:23 -0800

    [Maps] Implement JS 'Transition' Class

---
+++ maps/js/entities/transition.js
@@ -0,0 +1,14 @@
+class Transition extends Phaser.Scene
+{
+  constructor(Name, Grid_Engine_ID, Properties, Type, Coords, Render_Instance)
+  {
+    super();
+
+    this.Name = Name;
+    this.Grid_Engine_ID = Grid_Engine_ID;
+    this.Render_Instance = Render_Instance;
+    this.properties = Properties;
+    this.type = Type;
+    this.coords = Coords;
+  }
+}
