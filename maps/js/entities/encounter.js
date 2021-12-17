bc2749017d57200f42387da7fed3b3d3759cd661
Jess <toxocious@gmail.com>
2021-12-16 16:00:38 -0800

    [Maps] Display encounter via Encounter class instead of Player class

--- maps/js/entities/encounter.js
+++ maps/js/entities/encounter.js
@@ -11,4 +11,36 @@ class Encounter extends Phaser.Scene
     this.type = Type;
     this.coords = Coords;
   }
+
+  /**
+   * Display the currently active encounter to the player.
+   */
+  DisplayEncounter(Steps_Till_Encounter, In_Encounter)
+  {
+    MapGame.Network.SendRequest('Encounter').then((Encounter) => {
+      Encounter = JSON.parse(Encounter);
+
+      if ( Encounter.Generated_Encounter.Type !== 'Normal' )
+        alert(`You found a wild ${Encounter.Generated_Encounter.Type} Pok&eacute;mon!`);
+
+      document.getElementById('map_dialogue').innerHTML = `
+        A wild <b>${Encounter.Generated_Encounter.Pokedex_Data.Display_Name}</b> appeared!
+        <br />
+        <img src='${Encounter.Generated_Encounter.Pokedex_Data.Sprite}' />
+        <br />
+        <b>${Encounter.Generated_Encounter.Pokedex_Data.Display_Name}</b>
+        ${Encounter.Generated_Encounter.Gender.charAt(0)}
+        (Level: ${Encounter.Generated_Encounter.Level.toLocaleString()})
+
+        <br /><br />
+
+        <div class='flex wrap' style='gap: 10px; justify-content: center; max-width: 240px;'>
+          <button style='flex-basis: 100px;' onclick='MapGame.Player.FightEncounter();'>Fight</button>
+          <button style='flex-basis: 100px;' onclick='MapGame.Player.CatchEncounter();'>Catch</button>
+          <button style='flex-basis: 100px;' onclick='MapGame.Player.ReleaseEncounter();'>Release</button>
+          <button style='flex-basis: 100px;' onclick='MapGame.Player.RunFromEncounter();'>Run</button>
+        </div>
+      `;
+    });
+  }
 }
--- maps/js/entities/player.js
+++ maps/js/entities/player.js
@@ -145,45 +145,22 @@ class Player_Entity
       this.Steps_Till_Encounter = data.Next_Encounter;
 
       if ( this.Steps_Till_Encounter === 0 )
-        this.CheckForEncounter();
+        this.CheckForEncounter(Tile_Info);
     });
   }
 
   /**
    * Check for a wild encounter.
    */
-  CheckForEncounter()
+  CheckForEncounter(Tile_Info)
   {
     if ( this.Steps_Till_Encounter !== 0 || MapGame.Player.In_Encounter )
       return;
 
     MapGame.Player.In_Encounter = true;
 
-    MapGame.Network.SendRequest('Encounter').then((Encounter) => {
-      Encounter = JSON.parse(Encounter);
-
-      if ( Encounter.Generated_Encounter.Type !== 'Normal' )
-        alert(`You found a wild ${Encounter.Generated_Encounter.Type} Pok&eacute;mon!`);
-
-      document.getElementById('map_dialogue').innerHTML = `
-        A wild <b>${Encounter.Generated_Encounter.Pokedex_Data.Display_Name}</b> appeared!
-        <br />
-        <img src='${Encounter.Generated_Encounter.Pokedex_Data.Sprite}' />
-        <br />
-        <b>${Encounter.Generated_Encounter.Pokedex_Data.Display_Name}</b>
-        ${Encounter.Generated_Encounter.Gender.charAt(0)}
-        (Level: ${Encounter.Generated_Encounter.Level.toLocaleString()})
-
-        <br /><br />
-
-        <div class='flex wrap' style='gap: 10px; justify-content: center; max-width: 240px;'>
-          <button style='flex-basis: 100px;'>Fight</button>
-          <button style='flex-basis: 100px;'>Catch</button>
-          <button style='flex-basis: 100px;'>Release</button>
-          <button style='flex-basis: 100px;' onclick='MapGame.Player.RunFromEncounter();'>Run</button>
-        </div>
-      `;
-    });
+    MapGame.Encounter = new Encounter(Tile_Info.Objects.Name, Tile_Info.Objects.Grid_Engine_ID, Tile_Info.Objects.properties, Tile_Info.Objects.type, Tile_Info.Objects.coords, Tile_Info.Objects.Render_Instance);
+    MapGame.Encounter.DisplayEncounter(this.Steps_Till_Encounter, MapGame.Player.In_Encounter);
   }
 
   /**
