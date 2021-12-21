b5a35935c0497d0901337d237d9a77057fedf7ed
Jess <toxocious@gmail.com>
2021-12-20 21:11:46 -0800

    [Battle] Implement method to generate fake roster

--- battles/classes/roster.php
+++ battles/classes/roster.php
@@ -3,6 +3,9 @@
 
   class Roster extends Battle
   {
+    /**
+     * Create a roster from Pokemon existing within the database.
+     */
     public function CreateRoster
     (
       $Side_ID,
@@ -34,9 +37,58 @@
 
       foreach ( $Roster as $Index => $Pokemon )
       {
-        $PokemonHandler[$Index] = new PokemonHandler($Pokemon['ID'], $Side, $Pokemon['Slot']);
+        $Pokemon_Handler[$Index] = new PokemonHandler($Pokemon['ID'], $Side, $Pokemon['Slot']);
       }
 
-      return $PokemonHandler;
+      return $Pokemon_Handler;
+    }
+
+    /**
+     * Create a roster from nonexistent Pokemon.
+     *
+     * @param {array} $Pokemon
+     * @param {string} $Side
+     */
+    public function CreateFakeRoster
+    (
+      array $Roster_Pokemon,
+      string $Side
+    )
+    {
+      if ( empty($Roster_Pokemon) )
+        return false;
+
+      if ( count($Roster_Pokemon) === 6 )
+      {
+        $Pokemon_Handler[0] = new PokemonHandler(
+          null,
+          $Side,
+          1,
+          $Roster_Pokemon['Pokedex_Data']['Pokedex_ID'],
+          $Roster_Pokemon['Pokedex_Data']['Alt_ID'],
+          $Roster_Pokemon['Level'],
+          $Roster_Pokemon['Type'],
+          $Roster_Pokemon['Gender']
+        );
+      }
+      else
+      {
+        foreach ( $Roster_Pokemon as $Slot => $Pokemon )
+        {
+          $Pokemon_Handler[$Slot] = new PokemonHandler(
+            null,
+            $Side,
+            $Slot,
+            $Pokemon['Pokedex_ID'],
+            $Pokemon['Alt_ID'],
+            $Pokemon['Level'],
+            $Pokemon['Type'],
+            $Pokemon['Gender']
+          );
+        }
+      }
+
+
+      return $Pokemon_Handler;
     }
   }
