9284a04d630b52897b57eaf6c072bd0bd092bc41
Jess <toxocious@gmail.com>
2022-01-21 12:25:38 -0800

    [Staff] Remove console logging & fix promise error

--- staff/js/sprite_list.js
+++ staff/js/sprite_list.js
@@ -1,10 +1,8 @@
 function ShowSprites(Category, Sub_Category = null)
 {
-  console.log('Showing sprites');
-
   SendRequest('sprite_list', { Category: Category, Sub_Category: Sub_Category })
     .then((Sprites) => {
       document.getElementById('Sprite_AJAX').innerHTML = Sprites;
     })
-    .catch((Error) => console.log('Error'));
+    .catch((Error) => console.log('Error:', Error));
 }
