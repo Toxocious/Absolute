0142dea1fc31c5bed455280a0a4969a9b8e8d12a
Jess <toxocious@gmail.com>
2022-01-29 13:55:02 -0800

    [Staff] Display amount of currently banned users

--- staff/pages/ban_list.php
+++ staff/pages/ban_list.php
@@ -17,6 +17,8 @@
   }
 
   require_once $_SERVER['DOCUMENT_ROOT'] . '/staff/functions/ban.php';
+
+  $Banned_Users = GetBannedUsers();
 ?>
 
 <div style='display: flex; flex-wrap: wrap; justify-content: center;'>
@@ -24,11 +26,15 @@
     <h3>Banned Users</h3>
   </div>
 
+  <div style='flex-basis: 100%; width: 100%;'>
+    <h4>There are <?= number_format(count($Banned_Users)); ?> banned users.</h3>
+  </div>
+
   <div id='Ban_AJAX'></div>
 
   <div style='display: flex; flex-wrap: wrap; gap: 10px;' id='Banned_User_List'>
     <?php
-      echo ShowBannedUsers(GetBannedUsers());
+      echo ShowBannedUsers($Banned_Users);
     ?>
   </div>
 </div>
