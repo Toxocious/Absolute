1f66b54d7acd5adb747555468ac7c54f6c4e4694
Jess <toxocious@gmail.com>
2022-02-13 15:16:43 -0800

    [Staff] Only include js files if the user has the correct power level

--- staff/functions/auth.php
+++ staff/functions/auth.php
@@ -1,4 +1,8 @@
 <?php
+  /**
+   * Check the user's power level in comparison to the page's power level.
+   * Allow access to the page if they user's power level is not below the required power level.
+   */
   function AuthorizeUser()
   {
     global $User_Data, $Current_Page;
--- staff/index.php
+++ staff/index.php
@@ -35,18 +35,33 @@
 </div>
 
 <script type='text/javascript' src='js/ajax_functions.js'></script>
-<script type='text/javascript' src='js/sprite_list.js'></script>
-<script type='text/javascript' src='js/log_system.js'></script>
-<script type='text/javascript' src='js/report.js'></script>
-<script type='text/javascript' src='js/ban.js'></script>
-<script type='text/javascript' src='js/modify_user.js'></script>
-<script type='text/javascript' src='js/modify_pokemon.js'></script>
-<script type='text/javascript' src='js/transfer_pokemon.js'></script>
-<script type='text/javascript' src='js/maintenance.js'></script>
-<script type='text/javascript' src='js/set_pokemon.js'></script>
-<script type='text/javascript' src='js/set_items.js'></script>
-<script type='text/javascript' src='js/edit_pokedex.js'></script>
-<script type='text/javascript' src='js/edit_item.js'></script>
 
 <?php
-	require_once $_SERVER['DOCUMENT_ROOT'] . '/core/required/layout_bottom.php';
+  switch ( $User_Data['Power'] )
+  {
+    case 7:
+    case 6:
+      echo "<script type='text/javascript' src='js/edit_pokedex.js'></script>";
+      echo "<script type='text/javascript' src='js/edit_item.js'></script>";
+
+    case 5:
+      echo "<script type='text/javascript' src='js/modify_user.js'></script>";
+      echo "<script type='text/javascript' src='js/modify_pokemon.js'></script>";
+      echo "<script type='text/javascript' src='js/transfer_pokemon.js'></script>";
+      echo "<script type='text/javascript' src='js/set_pokemon.js'></script>";
+      echo "<script type='text/javascript' src='js/set_items.js'></script>";
+
+    case 4:
+      echo "<script type='text/javascript' src='js/maintenance.js'></script>";
+
+    case 3:
+      echo "<script type='text/javascript' src='js/log_system.js'></script>";
+      echo "<script type='text/javascript' src='js/report.js'></script>";
+      echo "<script type='text/javascript' src='js/ban.js'></script>";
+
+    case 2:
+      echo "<script type='text/javascript' src='js/sprite_list.js'></script>";
+      break;
+  }
+
+  require_once $_SERVER['DOCUMENT_ROOT'] . '/core/required/layout_bottom.php';
