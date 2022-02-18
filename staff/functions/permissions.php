fa93f6c857ac3c125eae8a1e8f76f220a8593f65
Jess <toxocious@gmail.com>
2022-02-18 01:53:07 -0800

    [Staff] Add permission descriptions

--- staff/functions/permissions.php
+++ staff/functions/permissions.php
@@ -1,20 +1,23 @@
 <?php
   const PERMISSIONS = [
-    'Staff',
-    'Graphics',
-    'Logs',
-    'Reports',
-    'Bans',
-    'User Management',
-    'Pokemon Management',
-    'Transfer Pokemon',
-    'Maintenance',
-    'Set Obtainables',
-    'Database Edits',
-    'Spawn',
-    'Staff Management'
+    'Staff' => 'Required to access the staff panel index page.',
+    'Graphics' => 'Grants access to the sprite list.',
+    'Logs' => 'Grants access to the log system.',
+    'Reports' => 'Grants access to all reports.',
+    'Bans' => 'Grants access to the ban system.',
+    'User Management' => 'Grants access to modify users.',
+    'Pokemon Management' => 'Grants access to modify user\'s Pok&eacute;mon.',
+    'Transfer Pokemon' => 'Grants access to transfer Pok&eacute;mon.',
+    'Maintenance' => 'Grants access to put pages in and out of maintenace.',
+    'Set Obtainables' => 'Grants access to set all obtainable items and Pok&eacute;mon.',
+    'Database Edits' => 'Grants access to edit base item, move, and Pok&eacute;dex values.',
+    'Spawn' => 'Grants access to the item and Pok&eacute;mon spawners.',
+    'Staff Management' => 'Grants access to the manage staff panel.',
   ];
 
+  /**
+   * Check if the user has the required permission for the page.
+   */
   function CheckUserPermission()
   {
     global $Current_Page, $PDO, $User_Data;
