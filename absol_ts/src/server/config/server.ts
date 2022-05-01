5083f12ce4b677dd5f521239367f2da8a7b3f665
Jess <toxocious@gmail.com>
2022-04-30 21:30:57 -0700

    [Chat] Remove WIP chat rewrite; moving to separate repo

--- absol_ts/src/server/config/server.ts
+++
@@ -1,3 +0,0 @@
-export const COMMAND_PREFIX = '~';
-export const SERVER_PORT = 8080;
-export const MESSAGE_PORT = 9001;
--- absol_ts/src/server/types/command.ts
+++
@@ -1,6 +0,0 @@
-export interface CommandInterface {
-  name: string;
-  description: string;
-  args?: any;
-  execute: Function;
-}
