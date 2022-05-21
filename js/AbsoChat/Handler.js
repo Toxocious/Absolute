d6eb9b7f3644ddcca84e0f9c59a58bf363fea64b
Jess <toxocious@gmail.com>
2022-05-21 14:52:20 -0700

    [Chat] Fix client-side chat logic

--- js/AbsoChat/Handler.js
+++ js/AbsoChat/Handler.js
@@ -6,35 +6,32 @@ const Handler = {
   /**
    * Clear the message array.
    */
-  Clear: function()
-  {
+  Clear: function () {
     this.Messages = [];
   },
 
   /**
    * Add a message to the messages array.
    */
-  AddMessage: function(message)
-  {
+  AddMessage: function (message) {
     /**
      * Only works if the socket and server are currently active.
      */
-    if ( Absolute.active )
-    {
+    if (Absolute.active) {
       let Message_Data;
 
       Message_Data = {
         User: {
-          ID: message.User.ID,
-          Name: message.User.Name,
-          Rank: message.User.Rank,
-          Avatar: message.User.Avatar,
+          ID: message.userID,
+          Name: message.userName,
+          Rank: message.userRank,
+          Avatar: message.userAvatar,
         },
         Message: {
           ID: this.Messages.length,
-          Content: message.Message.Content,
-          Timestamp: message.Message.Timestamp ? message.Message.Timestamp : new Date().toLocaleTimeString("en-US"),
-          Private: message.Message.Private,
+          Content: message.messageText,
+          Timestamp: new Date(message.sentOn).toLocaleTimeString('en-US'),
+          Private: message.isPrivate,
         },
       };
 
@@ -47,8 +44,7 @@ const Handler = {
   /**
    * Display messages in the chat window.
    */
-  Display: function()
-  {
+  Display: function () {
     /**
      * Reset the currently stored message HTML so that we don't get repeated chat messages.
      */
@@ -58,8 +54,7 @@ const Handler = {
      * The client didn't connect to the chat for one reason or another.
      * Display that Absol is likely offline.
      */
-    if ( typeof Absolute.user.connected === 'undefined' )
-    {
+    if (typeof Absolute.user.connected === 'undefined') {
       Handler.Message = `
         <table style="width: 100%; height: 100%;">
           <tr>
@@ -79,40 +74,38 @@ const Handler = {
      * Determine how many messages to loop through.
      */
     let Iterations = 0;
-    if ( this.Messages.length < this.MaxMessages )
+    if (this.Messages.length < this.MaxMessages)
       Iterations = this.Messages.length;
-    else
-      Iterations = this.MaxMessages;
+    else Iterations = this.MaxMessages;
 
     /**
      * Loop through the messages array.
      */
-    for ( let i = 0; i < Iterations; i++ )
-      this.RenderMessage( this.Messages[i] );
+    for (let i = 0; i < Iterations; i++) this.RenderMessage(this.Messages[i]);
 
     $('#AbsoChat').find('#chatContent').first().html(Handler.Message);
-    $('#chatContent').scrollTop( $('#chatContent')[0].scrollHeight );
+    $('#chatContent').scrollTop($('#chatContent')[0].scrollHeight);
   },
 
   /**
    * Render a message.
    */
-  RenderMessage: function(message)
-  {
-    if ( typeof message !== 'undefined' )
-    {
+  RenderMessage: function (message) {
+    if (typeof message !== 'undefined') {
       const Message_Data = {
         User: {
           ID: message.User.ID,
           Username: message.User.Name,
-          Avatar: message.User.Avatar ? message.User.Avatar : `https://${location.hostname}/images/Pokemon/Sprites/0.png`,
+          Avatar: message.User.Avatar
+            ? message.User.Avatar
+            : `https://${location.hostname}/images/Pokemon/Sprites/0.png`,
           Rank: message.User.Rank,
         },
         Message: {
           ID: message.Message.ID,
           Content: message.Message.Content,
           Timestamp: message.Message.Timestamp,
-          Private: message.Message.Private
+          Private: message.Message.Private,
         },
       };
 
@@ -122,12 +115,14 @@ const Handler = {
        * - Private messages that were only sent to the user.
        */
       if (
-        !Message_Data.Message.Private.isPrivate || 
-        ( Message_Data.Message.Private.isPrivate && Message_Data.Message.Private.Private_To === Absolute.user.user_id )
-      )
-      {
+        !Message_Data.Message.Private.isPrivate ||
+        (Message_Data.Message.Private.isPrivate &&
+          Message_Data.Message.Private.Private_To === Absolute.user.user_id)
+      ) {
         Handler.Message += `
-          <div class="message${(Message_Data.Message.Private.isPrivate ? ' private' : '')}" data-msg-id="${Message_Data.Message.ID}">
+          <div class="message${
+            Message_Data.Message.Private.isPrivate ? ' private' : ''
+          }" data-msg-id="${Message_Data.Message.ID}">
             <div class="avatar">
               <a href='/profile.php?id=${Message_Data.User.ID}'>
                 <img src="/images/${Message_Data.User.Avatar}" />
@@ -135,7 +130,9 @@ const Handler = {
             </div>
             <div class="username">
               <a href='/profile.php?id=${Message_Data.User.ID}'>
-                <b class='${Message_Data.User.Rank}'>${Message_Data.User.Username}</b>
+                <b class='${Message_Data.User.Rank}'>${
+          Message_Data.User.Username
+        }</b>
               </a>
               <br />
               ${Message_Data.Message.Timestamp}
--- js/AbsoChat/absochat.js
+++ js/AbsoChat/absochat.js
@@ -6,29 +6,24 @@ const Absolute = {
   user: {},
   messages: {},
 
-  isConnected: function()
-  {
+  isConnected: function () {
     return socket.connected;
   },
 
-  handleInputBox: function()
-  {
-    if ( !Absolute.user.connected )
+  handleInputBox: function () {
+    if (!Absolute.user.connected)
       $('#chatMessage').css('background', '#666').attr('disabled', true);
-    else
-      $('#chatMessage').css('background', '').attr('disabled', false);
+    else $('#chatMessage').css('background', '').attr('disabled', false);
   },
 
   /**
    * Initialization of Absolute Chat.
    */
-  Enable: function()
-  {
+  Enable: function () {
     /**
      * Structure the socket commands.
      */
-    if ( typeof socket != 'object' )
-    {
+    if (typeof socket != 'object') {
       /**
        * Create new array of messages.
        */
@@ -37,62 +32,60 @@ const Absolute = {
       /**
        * Start the connection to the server.
        */
-      socket = io('https://www.absoluterpg.com:' + Absolute.port,
-      {
+      // socket = io('https://www.absoluterpg.com:' + Absolute.port,
+      socket = io('http://localhost:8080', {
+        withCredentials: true,
         reconnectionDelay: 2000,
-        reconnectionAttempts: 30,
+        reconnectionAttempts: 15,
         reconnection: true,
-        secure: true
+        secure: true,
       });
 
       /**
        * Handle the connection of the user.
        */
-      socket.on("connect", function()
-      {
+      socket.on('connect', function () {
         Absolute.active = true;
         Absolute.user.connected = true;
+
+        console.log('[Chat | Client] Connected to socket.');
 
         Handler.Clear();
         Absolute.handleInputBox();
 
-        socket.emit('auth',
-        {
+        socket.emit('auth', {
           user: Absolute.user.user_id,
-          postcode: Absolute.user.postcode
+          postcode: Absolute.user.postcode,
         });
       });
 
       /**
        * Handle the user upon disconnecting.
        */
-      socket.on("disconnect", function()
-      {
+      socket.on('disconnect', function () {
         Absolute.handleInputBox();
-        Handler.AddMessage(
-          {
-            User: {
-              ID: 3,
-              Name: 'Absol',
-              Rank: 'bot',
-              Avatar: '../images/Avatars/Custom/3.png',
-            },
-            Message: {
-              Content: 'You have been disconnected from Absolute Chat. Please refresh the page.',
-              Private: {
-                isPrivate: true,
-                Private_To: Absolute.user.user_id,
-              },
-            }
+        Handler.AddMessage({
+          User: {
+            ID: 3,
+            Name: 'Absol',
+            Rank: 'bot',
+            Avatar: '../images/Avatars/Custom/3.png',
           },
-        );
+          Message: {
+            Content:
+              'You have been disconnected from Absolute Chat. Please refresh the page.',
+            Private: {
+              isPrivate: true,
+              Private_To: Absolute.user.user_id,
+            },
+          },
+        });
       });
 
       /**
        * Handle sent messages.
        */
-      socket.on('chat-message', function(data)
-      {
+      socket.on('chat-message', function (data) {
         Handler.AddMessage(data);
       });
     }
@@ -103,8 +96,7 @@ const Absolute = {
   /**
    * Disabling of Absolute Chat.
    */
-  Disable: function()
-  {
+  Disable: function () {
     Absolute.active = false;
   },
 };
