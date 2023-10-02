bab04098489f001390c3019b0c47560f08a2e3a6
Toxocious <toxocious@gmail.com>
2023-10-02 16:48:18 -0700

    refactor start.sh

--- start.sh
+++ start.sh
@@ -1,45 +1,84 @@
 #!/bin/bash
 
-# Check for dev server certs.
-if [ ! -f "certbot/conf/live/absoluterpg.com/fullchain.pem" ]; then
+# CLI flags
+verbose_migrations=false
+
+# Function to display an error message and exit
+error_exit() {
+  echo "[ERROR] > $1"
+  exit 1
+}
+
+# Function to generate development certificates using Certbot
+generate_dev_certs() {
+  if [ ! -f "certbot/conf/live/absoluterpg.com/fullchain.pem" ]; then
     echo "[INFO] Dev certs do not exist. Running certbot script."
+    bash certbot/generate.sh
 
-    certs=$(bash certbot/generate.sh)
-    echo "$certs"
-
-    if [[ "$certs" != *"[SUCCESS] Succesfully enerated development certifications."* ]]; then
-        echo "[ERROR] Wasn't able to generate certs for the development server. Exiting."
-        exit 1
+    if [ $? -ne 0 ]; then
+      error_exit "Failed to generate development certificates."
     fi
-fi
+  fi
+}
 
-file="logs/last-commit"
-current_commit=$(git rev-parse --short HEAD)
+# Function to build Docker containers
+build_docker_containers() {
+  file="logs/last-commit"
+  current_commit=$(git rev-parse --short HEAD)
 
-# Build the docker containers
-if [ "$1" = "--build" ] || [ ! -f "$file" ] || [ "$current_commit" != "$(cat $file)" ]; then
+  if [ "$1" = "--build" ] || [ ! -f "$file" ] || [ "$current_commit" != "$(cat $file)" ]; then
+    echo "[INFO] Building Docker containers."
     docker-compose build
+    echo "$current_commit" > "$file"
+  else
+    echo "[NOTICE] Already built, not running build script."
+  fi
+}
 
-    echo $current_commit > $file
-else
-    echo "[NOTICE] Already built, not running build script"
-fi
+# Function to start Docker containers
+start_docker_containers() {
+  echo "[INFO] Starting Docker containers in the background."
+  docker-compose up -d
 
-# Start the containers in the background
-docker-compose up -d
+  if [ $? -ne 0 ]; then
+    error_exit "Docker compose build failed."
+  fi
+}
 
-# Check the exit status of docker-compose
-if [[ $? -ne 0 ]]; then
-    echo "[ERROR] Docker compose build failed. Exiting."
-    exit 1
-fi
+# Function to execute SQL migrations inside the MySQL Docker container
+execute_sql_migrations() {
+  echo "[INFO] Executing SQL migrations."
+  migrations=$(docker exec -it absolute-mysql bash -c "/data/application/migrate.sh")
 
-# Execute SQL migrations inside of the mysql docker container
-migrations=$(docker exec -it absolute-mysql bash -c "/data/application/migrate.sh")
-echo "$migrations"
+  if [[ "$migrations" != *"[SUCCESS] All migrations executed successfully."* ]]; then
+    error_exit "Migrations failed."
+  else
+    echo " > Migrations ran successfully."
 
-if [[ "$migrations" != *"[SUCCESS] All migrations executed successfully."* ]]; then
-    echo "[ERROR] Migrations failed."
-fi
+    if [ "$verbose_migrations" = true ]; then
+      echo "[LOGS // MIGRATION] $migrations"
+    fi
+  fi
+}
+
+# Parse command-line flags
+while getopts "m" flag; do
+  case $flag in
+    m) verbose_migrations=true ;;
+    *) exit 1 ;;
+  esac
+done
+
+# Check for development server certificates
+generate_dev_certs
+
+# Build Docker containers
+build_docker_containers "$1"
+
+# Start Docker containers
+start_docker_containers
+
+# Execute SQL migrations
+execute_sql_migrations
 
 echo "[SUCCESS] Server successfully started."
