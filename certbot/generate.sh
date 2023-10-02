c199ee77e1717c311d474c33b841530f35fba9ae
Toxocious <toxocious@gmail.com>
2023-10-02 12:31:16 -0700

    fix: ssl generation script

--- certbot/generate.sh
+++ certbot/generate.sh
@@ -5,16 +5,15 @@
 cd "$(dirname "$0")"
 
 mkdir -p www
-mkdir -p conf/live/absoluterpg.net
-cd conf/live/absoluterpg.net
-
-# apt-get install -y openssl
-# pacman -S openssl
+mkdir -p conf/live/absoluterpg.com
+cd conf/live/absoluterpg.com
 
 openssl genrsa -des3 -passout pass:x -out server.pass.key 4096
 openssl rsa -passin pass:x -in server.pass.key -out privkey.pem
 
 rm server.pass.key
 
-openssl req -new -key privkey.pem -out server.csr -subj "/C=US/ST=/L=/O=Toxocious/OU=IT Department/CN=absoluterpg.net"
+openssl req -new -key privkey.pem -out server.csr -subj "/C=US/ST=/L=/O=Toxocious/OU=IT Department/CN=absoluterpg.com"
 openssl x509 -req -days 3650 -in server.csr -signkey privkey.pem -out fullchain.pem
+
+echo "[SUCCESS] Succesfully generated development certifications."
