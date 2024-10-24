28ca6da73ce957b6bc0aa3d78537d624736101f6
Jesse Mack <toxocious@gmail.com>
2024-10-23 22:40:08 -0700

    docker: fix ssl certificate generation script

--- certbot/generate.sh
+++ certbot/generate.sh
@@ -2,18 +2,27 @@
 
 # Generates https certificates to use during development
 
-cd "$(dirname "$0")"
+if ! [ -x "$(command -v openssl)" ]; then
+    if (( $EUID != 0 )); then
+        echo "Please run as root"
+        exit
+    fi
 
-mkdir -p www
-mkdir -p conf/live/absoluterpg.com
-cd conf/live/absoluterpg.com
+    apt-get install -y openssl
+fi
 
-openssl genrsa -des3 -passout pass:x -out server.pass.key 4096
+DIR="$(dirname "$0")"
+
+mkdir -p "$DIR/conf/live/absoluterpg.com" "$DIR/www"
+
+cd "$DIR/conf/live/absoluterpg.com/"
+
+openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
 openssl rsa -passin pass:x -in server.pass.key -out privkey.pem
 
 rm server.pass.key
 
-openssl req -new -key privkey.pem -out server.csr -subj "/C=US/ST=/L=/O=Toxocious/OU=IT Department/CN=absoluterpg.com"
+openssl req -new -key privkey.pem -out server.csr -subj "/C=US/ST=RPG/L=Toxocious's/O=Absolute RPG/OU=IT Department/CN=absoluterpg.com"
 openssl x509 -req -days 3650 -in server.csr -signkey privkey.pem -out fullchain.pem
 
 echo "[SUCCESS] Succesfully generated development certifications."
