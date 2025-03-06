5e87935da7c8cf2f7cf882ca6cd03f802fb144c2
Jess <toxocious@gmail.com>
2025-03-05 23:48:43 -0700

    theme: fix latest news post styling

--- app/news.php
+++ app/news.php
@@ -38,30 +38,40 @@
   {
 ?>
 
-<table class='border-gradient'<?= $style; ?>>
-	<thead>
-		<tr>
-			<th colspan='2'>
-				<?= $News_Post['News_Title']; ?>
-			</th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<td style='padding: 5px 30px; width: 150px;'>
-				<img src='<?= DOMAIN_SPRITES . '/' . $News_Poster['Avatar']; ?>' /><br />
-				<?php
-					echo '<h3>' . $User_Class->DisplayUserName($News_Post['Poster_ID'], false, false, true) . '</h3>';
-					echo $News_Post['News_Date'];
-				?>
-			</td>
+    <div class='panel content'>
+      <div class='head'>News</div>
+      <div class='body' style='padding: 1em;'>
+        <div style='margin: 0 1em 1em;'>
 
-			<td style='padding: 10px;'>
-				<?= html_entity_decode($News_Post['News_Text']); ?>
-			</td>
-		</tr>
-	</tbody>
-</table>
+          Stay up to date on the latest news and updates from the staff team.
+        </div>
+
+        <table class='border-gradient' style='width: 100%;'>
+          <thead>
+            <tr>
+              <th colspan='2'>
+                <?= $News_Post['News_Title']; ?>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td style='padding: 5px 30px; width: 150px;'>
+                <img src='<?= DOMAIN_SPRITES . '/' . $News_Post['Avatar']; ?>' /><br />
+                <?php
+                  echo '<h3>' . $User_Class->DisplayUserName($News_Post['Poster_ID'], false, false, true) . '</h3>';
+                  echo $News_Post['News_Date'];
+                ?>
+              </td>
+
+              <td style='padding: 10px;'>
+                <?= html_entity_decode($News_Post['News_Text']); ?>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
 
 <?php
   }
